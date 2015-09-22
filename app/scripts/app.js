78905f442c753f0e07cc3aa816fa467fd3593378
Samual Holmes <baldwisamu1@old-bailey.ecs.vuw.ac.nz>
2015-09-23 11:00:10 +1200

    Created Walker GUI

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,17 +14,7 @@
      * Compile the code in the text editor.
      * Create and display the new automatas.
      */
-<<<<<<< HEAD
-    app.compile = function(){
-    //  var automatas = app.$.parser.parse(app.$.editor.getCode());
 
-      // Can't simply assign app.data.automatas to the new array as data bindings will not update.
-      // Creating a new data oject then setting the automatas value slighly later will work (for some reason).
-    //  app.data = {};
-    //  this.async(function(){
-    //    app.set('data.automatas', automatas);
-   //   });
-=======
     app.compile = function() {
       var automatas = app.$.parser.parse(app.$.editor.getCode());
 
@@ -34,7 +24,6 @@
       this.async(function() {
         app.set('data.automatas', automatas);
       });
->>>>>>> 5139cab04aa504ba777eb8d3bc75c462376a4717
     };
 
     /**
@@ -68,32 +57,3 @@
   });
 })(document);
 
-function createWalker(){
-  //Check which visualisation
-  //Set walking boolean to true.
-  //Set first node to new color.
-  //Check for choice
-
-}
-
-function nextNode(){
-  alert('nextNode')
-  //If walking boolean false do nothing.
-  //If no more nodes to step through do nothing.
-  //Change color of the next node down the given path.
-
-}
-
-function checkChoice(){
-  //For all the edges.
-  //If there are more than one edge with the same current from.
-  //Set hasChoice boolean to true.
-  //Add them to a temporary list.
-  //Save the current edge somewhere
-  //Change the current edge to the new color.
-}
-
-function changeChoice(){
-  //If hasChoice is false do nothing.
-
-}
