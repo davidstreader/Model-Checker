1aba87384cc5a805fbb0d7c6d2b906d58075f8f2
slaymaster3000 <slaymaster3000@gmail.com>
2015-10-14 12:48:59 +1300

    refixed double render thing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -100,7 +100,7 @@
         var reader = new FileReader();
         reader.onload = function() {
           var text = reader.result;
-          app.$.editor.setCode(text, app.liveCompiling);
+          app.$.editor.setCode(text);
           app.$.editor.focus();
         };
         reader.readAsText(input.files[0]);
