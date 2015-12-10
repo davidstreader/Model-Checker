16f593517dc141102bf4cd643b440bfbf0be12c5
David Sheridan <dpmsheridan@hotmail.com>
2015-12-10 15:26:46 +1300

    Implemented an equivalence function into the interpreter which can be used to check if two definitions are equivalent. The bisimulation function in graph operations was updated to accomodate multiple graphs so that the new simplification and isEquivalent functions could both use the function. Currently the equivalence result is printed to the console.

--- app/scripts/graph.es6.js
+++ app/scripts/graph.es6.js
@@ -423,7 +423,6 @@ class Graph {
         node.addMetaData('startNode', true);
       }
     }
-    console.log(this._nodeMap);
   }
 
   /**
@@ -1384,18 +1383,70 @@ Graph.Operations = class {
   }
 
   /**
-   * Performs a bisimulation coloring on the specified graph, which gives each node in the graph a colouring
-   * based on transitions it makes to neighbouring nodes. Once the colouring is completed any nodes
-   * with the same colour are considered equivalent and are merged together.
+   * Constructs and returns a simplifed version of the specified graph by performing a 
+   * bisimulation colouring of the graph an merging the nodes that are of the same color.
+   *
+   * @param {!object} graph - the graph to be simplified
+   * @returns {!object} - a simplified version of the graph
    */
-  static bisimulation(graph) {
-    var clone = graph.deepClone();
+  static simplification(graph) {
+    // perform the bisimulation
+    var result = this._bisimulation([graph]);
+    return result.graphs[0];
+  }
+
+  /**
+   * Determines if the specified graphs are equivalent by performing a bisimulation colouring
+   * for each graph and comparing the colours of the root of each graph. If the roots from each graph
+   * all have the same colour then all the specified graphs are equivalent.
+   *
+   * @param {!array} graphs - an array of graphs to check for equivalency
+   * @returns {!boolean} - true if all graphs are equivalent, otherwise false.
+   */
+  static isEquivalent(...graphs) {
+    // perform the bisimulataion
+    var result = this._bisimulation(graphs);
+
+    // compare the colors of each root node
+    var coloredNodes = result.coloredNodes;
+    for(var i = 0; i < graphs.length - 1; i++){
+      var rootId1 = graphs[i].rootId;
+      var rootId2 = graphs[i + 1].rootId;
+      
+      // if root nodes do not match the graphs are not equivalent
+      if(coloredNodes[rootId1].color !== coloredNodes[rootId2].color){
+        return false;
+      }
+    }
+
+    // if this point is reached then all graphs were equivalent
+    return true;
+  }
+
+  /**
+   * Performs a bisimulation coloring on the specified graphs, which gives the nodes in each graph a colouring
+   * based on transitions it makes to neighbouring nodes. Once the colouring is completed any nodes
+   * within the same graph with the same colour are considered equivalent and are merged together.
+   *
+   * @private
+   * @param {!array} - an array of graphs to perform bisimulation colouring on.
+   * @returns {!object} - the coloured nodes and the simplified graphs
+   */
+  static _bisimulation(graphs) {
+    var clones = [];
+    for(let i in graphs){
+      clones.push(graphs[i].deepClone());
+    }
+
     // construct map of nodes and give them all the same color
     var coloredNodes = [];
-    var nodes = clone.nodes;
-    for(let n in nodes){
-      var node = nodes[n];
-      coloredNodes[node.id] = new Graph.ColoredNode(node);
+    for(let i in clones){
+      var clone = clones[i];
+      var nodes = clone.nodes;
+      for(let n in nodes){
+        var node = nodes[n];
+        coloredNodes[node.id] = new Graph.ColoredNode(node);
+      }
     }
 
     // continue process until color map does not increase in size
@@ -1408,23 +1459,31 @@ Graph.Operations = class {
     }
 
     // merge nodes together that have the same colors
-    for(let i in colorMap){
-      var nodeIds = [];
-      for(let j in coloredNodes){
-        var node = coloredNodes[j];
-        if(node.color === i){
-          nodeIds.push(node.node.id);
+    for(let i in clones){
+      var clone = clones[i];
+      var nodes = clone.nodes;
+      
+      for(let i in colorMap){
+        var nodeIds = [];
+        
+        for(let k in nodes){
+          var node = coloredNodes[nodes[k].id];
+          if(node.color === i){
+            nodeIds.push(node.node.id);
+          }
         }
-      }
-      if(nodeIds.length > 1){
-        clone.mergeNodes(nodeIds);
+        if(nodeIds.length > 1){
+          clone.mergeNodes(nodeIds);
+        }
       }
     }
 
     // remove duplicate edges
-    clone.removeDuplicateEdges();
+    for(let i in clones){
+      clones[i].removeDuplicateEdges();
+    }
 
-    return clone;
+    return {coloredNodes: coloredNodes, graphs: clones};
   }
 
   /**
