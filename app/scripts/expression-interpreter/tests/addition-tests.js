4b47dc2e03cf2abfbdb265d55be574ed56863af7
David Sheridan <dpmsheridan@hotmail.com>
2016-05-13 13:53:56 +1200

    Added more addition tests.

--- app/scripts/expression-interpreter/tests/addition-tests.js
+++ app/scripts/expression-interpreter/tests/addition-tests.js
@@ -28,13 +28,78 @@ describe('Addition Tests', function(){
 	});
 
 	it('The expression \'3 + (2 + 1)\' should equal 6', function(){
-		var result = evaluate("3 + (2 + 1)");
+		var result = evaluate('3 + (2 + 1)');
 		assert.equal(6, result);
 	});
 
 	it('The expression \'(3 + 2 + 1)\' should equal 6', function(){
-		var result = evaluate("(3 + 2 + 1)");
+		var result = evaluate('(3 + 2 + 1)');
 		assert.equal(6, result);
+	});
+
+	it('The expression \'3 - 2\' should equal 1', function(){
+		var result = evaluate('3 - 2');
+		assert.equal(1, result);
+	});
+
+	it('The expression \'5 - 2 - 1\' should equal 2', function(){
+		var result = evaluate('5 - 2 - 1');
+		assert.equal(2, result);
+	});	
+
+	it('The expression \'(5 - 2) - 1\' should equal 2', function(){
+		var result = evaluate('(5 - 2) - 1');
+		assert.equal(2, result);
+	});
+
+	it('The expression \'5 - (2 + 1)\' should equal 2', function(){
+		var result = evaluate('5 - (2 + 1)');
+		assert.equal(2, result);
+	});
+
+	it('The expression \'5 - (2 - 1)\' should equal 4', function(){
+		var result = evaluate('5 - (2 - 1)');
+		assert.equal(4, result);
+	});
+
+	it('The expression \'(5 - 2 - 1)\' should equal 2', function(){
+		var result = evaluate('(5 - 2 - 1)');
+		assert.equal(2, result);
+	});
+
+	it('The expression \'((5 - 2 - 1))\' should equal 2', function(){
+		var result = evaluate('((5 - 2 - 1))');
+		assert.equal(2, result);
+	});								
+
+	it('The expression \'10 + 2 - 7\' should equal 5', function(){
+		var result = evaluate('10 + 2 - 7');
+		assert.equal(5, result);
+	});
+
+	it('The expression \'(10 + 2) - 7\' should equal 5', function(){
+		var result = evaluate("(10 + 2) - 7");
+		assert.equal(5, result);
+	});
+
+	it('The expression \'10 + (2 - 7)\' should equal 5', function(){
+		var result = evaluate('10 + (2 - 7)');
+		assert.equal(5, result);
+	});
+
+	it('The expression \'10 - (2 - 7)\' should equal 15', function(){
+		var result = evaluate('10 - (2 - 7)');
+		assert.equal(15, result);
+	});
+
+	it('The expression \'(10 + 5) - (3 + 2)\' should equal 10', function(){
+		var result = evaluate('(10 + 5) - (3 + 2)');
+		assert.equal(10, result);
+	});
+
+	it('The expression \'(10 + (5 - 3) + 2)\' should equal 14', function(){
+		var result = evaluate('(10 + (5 - 3) + 2)');
+		assert.equal(14, result);
 	});
 
 });
