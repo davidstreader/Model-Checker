92028d4a059bdb6450ccfc75651f9c97212007fb
David Sheridan <dpmsheridan@hotmail.com>
2016-07-18 14:04:53 +1200

    Added another unit test for sequential petri net interpretation

--- app/scripts/compiler/tests/sequential-process-tests.js
+++ app/scripts/compiler/tests/sequential-process-tests.js
@@ -46,4 +46,24 @@ describe('Sequential Petri Net Interpretation Tests', function(){
 		assert(result, 'processes should be identical');
 	});
 
+	it('Should interpret petrinet A = (a -> b -> c -> d -> e -> STOP). correctly', function(){
+		// construct expected petri net
+		var places = generatePlaces(6, 0);
+		var transitions = generateTransitions(['a', 'b', 'c', 'd', 'e'], 0);
+		constructSequentialPetriNet(places, transitions);
+		var placeMap = convertArrayToMap(places);
+		var transitionMap = convertArrayToMap(transitions);
+		var expected = new PetriNet('test', placeMap, 6, transitionMap, {}, 5, ['test.0']);
+		
+		// construct received petri net
+		var processesMap = constructProcessesMap('petrinet A = (a -> b -> c -> d -> e -> STOP).');
+
+		// compare the received petri net with the expected petri net
+		var id = 0;
+		var graph1 = breadthFirstTraversal(id++, expected);
+		var graph2 = breadthFirstTraversal(id, processesMap['A']);
+		var result = compareBFTGraphs(graph1, graph2);
+		assert(result, 'processes should be identical');
+	});
+
 });
