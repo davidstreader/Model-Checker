30a7af885e1c5ea8c6d4ff1e554378a01b2dccca
David Sheridan <dpmsheridan@hotmail.com>
2016-07-20 14:42:33 +1200

    Re-implemented the parallel composition algorithm for automata

--- app/scripts/process-operations/parallel-composition.js
+++ app/scripts/process-operations/parallel-composition.js
@@ -18,7 +18,79 @@ function parallelComposition(id, process1, process2){
 	}
 
 	function automataParallelComposition(id, automaton1, automaton2){
-		// TODO
+		var graph = new Graph(id);
+		var nodes1 = automaton1.nodes;
+		var nodes2 = automaton2.nodes;
+		combineStates(graph, nodes1, nodes2);
+		var alphabet1 = automaton1.alphabet;
+		var alphabet2 = automaton2.alphabet;
+		var alphabet = alphabetUnion(alphabet1, alphabet2);
+
+		for(var i = 0; i < nodes1.length; i++){
+			var node1 = nodes1[i];
+			for(var j = 0; j < nodes2.length; j++){
+				var node2 = nodes2[j];
+
+				// the id for the current combined state
+				var fromId = node1.id + '.' + node2.id;
+
+				for(var action in alphabet){
+					var coaccessible1 = node1.coaccessible(action);
+					coaccessible1 = (coaccessible1.length !== 0) ? coaccessible1 : [undefined];
+					var coaccessible2 = node2.coaccessible(action);
+					coaccessible2 = (coaccessible2.length !== 0) ? coaccessible2 : [undefined];
+
+					for(var x = 0; x < coaccessible1.length; x++){
+						var c1 = coaccessible1[x];
+						for(var y = 0; y < coaccessible2.length; y++){
+							var c2 = coaccessible2[y];
+
+							if(c1 !== undefined && c2 !== undefined){
+								var toId = c1 + '.' + c2;
+								graph.addEdge(graph.nextEdgeId, action, fromId, toId);
+							}
+							else if(c1 !== undefined && alphabet2[action] === undefined){
+								var toId = c1 + '.' + node2.id;
+								graph.addEdge(graph.nextEdgeId, action, fromId, toId);
+							}
+							else if(c2 !== undefined && alphabet1[action] === undefined){
+								var toId = node1.id + '.' + c2;
+								graph.addEdge(graph.nextEdgeId, action, fromId, toId);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		// remove unreachable nodes from the composition
+		graph.trim();
+		return graph;
+
+		/**
+		 * Helper function for automataParallelComposition that creates a new
+		 * node in the specified automaton for each combined state from the specified
+		 * node arrays. Each node from the first array is combined with each node from
+		 * the second array.
+		 *
+		 * @param {automaton} graph - the automaton to add nodes to
+		 * @param {node[]} nodes1 - the first array of nodes
+		 * @param {node[]} nodes2 - the second array of nodes
+		 */
+		function combineStates(graph, nodes1, nodes2){
+			for(var i = 0; i < nodes1.length; i++){
+				for(var j = 0; j < nodes2.length; j++){
+					var id = nodes1[i].id + '.' + nodes2[j].id;
+					var metaData = metaDataIntersection(nodes1[i].metaData, nodes2[j].metaData);
+					graph.addNode(id, '', metaData);
+
+					// check if this is the first node constructed
+					if(i === 0 && j === 0){
+						graph.rootId = id;
+					}
+				}
+			}
+		}
 	}
 
 	/**
@@ -116,5 +188,57 @@ function parallelComposition(id, process1, process2){
 				transition1.addIncomingPlace(places[i]);
 			}
 		}
+	}
+
+	/**
+	 * The following are helper functions that can be utilised by parallel
+	 * composition functions.
+	 */
+
+	/**
+	 * Helper function for parallel composition functions that creates
+	 * and returns a union of the data stored in the two alphabet sets.
+	 *
+	 * @param {string{}} alphabet1 - the first alphabet set
+	 * @oaram {string{}} alphabet2 - the second alphabet set
+	 * @return {string{}} - the unioned alphabet set
+	 */
+	function alphabetUnion(alphabet1, alphabet2){
+		var alphabet = {};
+		// add actions from first alphabet
+		for(var action in alphabet1){
+			alphabet[action] = true;
+		}
+
+		// add actions from second alphabet
+		for(var action in alphabet2){
+			alphabet[action] = true;
+		}
+
+		return alphabet;
+	}
+
+	/**
+	 * Helper function for parallel composition functions that creates
+	 * and returnds an intersection of the data stored in two meta data objects.
+	 *
+	 * @param {object} metaData1 - first meta data set
+	 * @param {object} metaData2 - second meta data set
+	 * @return {object} - the intersected meta data
+	 */
+	function metaDataIntersection(metaData1, metaData2){
+		var metaData = {};
+		// check if there are any matching keys between the two meta data sets
+		for(var key in metaData1){
+			// check if there is a match
+			if(metaData2[key] !== undefined){
+				// check that the value stored is the same for both sets
+				if(metaData1[key] === metaData2[key]){
+					metaData[key] = metaData1[key];
+				}
+			}
+		}
+
+		return metaData;
 	}
 }
