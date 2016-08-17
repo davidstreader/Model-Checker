c9dd775da673ac934b4f8b361b227233e0ebc1ec
David Sheridan <dpmsheridan@hotmail.com>
2016-08-17 22:28:09 +1200

    Fixed issues occuring during the bisimulation algorithm

--- app/scripts/process-operations/bisimulation.js
+++ app/scripts/process-operations/bisimulation.js
@@ -13,54 +13,68 @@ function bisimulation(process){
 	}
 
 	function automataBisimulation(process){
-		// construct a coloured node map
-		var nodeMap = constructColouredNodeMap(process.nodes);
-		var colourMap = constructColourMap();
+		var nodeMap = colourNodes(process.nodes);
 		var lastColourCount = 0;
-		var colourCount = 1; // colour map is initialised with one colour
-		
+		var colourCount = 1;
+
+		var nodeColours;
 		while(lastColourCount <= colourCount){
-			var fringe = process.terminals;
+			var colourMap = {};
 			var visited = {};
+			nodeColours = {};
+			nodeColours[0] = []; // for terminals
 			var index = 0;
+			colourCount = 1;
+			var fringe = [process.root];
 			
 			while(index < fringe.length){
 				var current = fringe[index++];
+
 				// check if the current node has already been visited
 				if(visited[current.id] !== undefined){
 					continue;
 				}
-				
-				if(current.getMetaData('isTerminal') === undefined){
-					// construct colouring for the current node
-					var colouring = constructColouring(current);
+				visited[current.id] = true;
 
-					// check if this colour already exists
-					var colourId = -1;
-					for(var id in colourMap){
-						var result = compareColours(colouring, colourMap[id]);
-						if(result){
-							colourId = id;
-							break;
-						}
-					}
+				// check if the current node is a terminal
+				if(current.getMetaData('isTerminal') !== undefined){
+					nodeColours[0].push(current);
+					continue;
+				}
 
-					// add colour to colour map if necessary
-					if(colourId !== -1){
-						nodeMap[current.id].colour = colourId;
-					}
-					else{
-						colourMap[colourCount] = colouring;
-						nodeMap[current.id].colour = colourCount++; 
+				var colouring = constructColouring(current, nodeMap);
+
+				// check if this colour already exists
+				var colourId = -1;
+				for(var id in colourMap){
+					var result = compareColourings(colouring, colourMap[id]);
+					if(result){
+						colourId = id;
+						break;
 					}
 				}
 
-				// mark current node as visited
-				visited[current.id] = true;
+				if(colourId === -1){
+					colourId = colourCount++;
+					colourMap[colourId] = colouring;
+					nodeColours[colourId] = [];
+				}
 
-				var edges = current.edgesToMe;
+				nodeColours[colourId].push(current);
+
+				var edges = current.edgesFromMe;
 				for(var i = 0; i < edges.length; i++){
-					fringe.push(process.getNode(edges[i].from));
+					if(visited[edges[i].to] === undefined){
+						fringe.push(process.getNode(edges[i].to));
+					}
+				}
+			}
+
+			// apply the colourings to the nodes
+			for(var colour in nodeColours){
+				var nodes = nodeColours[colour];
+				for(var i = 0; i < nodes.length; i++){
+					nodeMap[nodes[i].id].colour =  colour;
 				}
 			}
 
@@ -72,127 +86,48 @@ function bisimulation(process){
 			lastColourCount = colourCount;
 		}
 
-		// group nodes by colour
-		var colourGroups = {};
-		for(var id in nodeMap){
-			var colour = nodeMap[id].colour;
-			if(colourGroups[colour] !== undefined){
-				colourGroups[colour].push(nodeMap[id].node);
-			}
-			else{
-				colourGroups[colour] = [nodeMap[id].node];
-			}
-		}
-
 		// merge nodes with the same colour
-		for(var colour in colourGroups){
-			process.mergeNodes(colourGroups[colour]);
+		for(var colour in nodeColours){
+			if(nodeColours[colour].length > 1){
+				process.mergeNodes(nodeColours[colour]);
+			}
 		}
 
 		process.removeDuplicateEdges();
 		return process;
 
-		/**
-		 * Constructs a colour map from the specified array of nodes. The mapping is
-		 * from the node id to a coloured node object, which is an object containing a
-		 * colour id and a node.
-		 *
-		 * @param {node[]} nodes - an array of nodes
-		 * @return {id -> colouredNode} - a mapping from node id to coloured node
-		 */
-		function constructColouredNodeMap(nodes){
+		function colourNodes(nodes){
 			var nodeMap = {};
 			for(var i = 0; i < nodes.length; i++){
-				nodeMap[nodes[i].id] = { colour:0, node:nodes[i] };
+				nodeMap[nodes[i].id] = { node:nodes[i], colour:0 };
 			}
 
 			return nodeMap;
 		}
 
-		/**
-		 * Constructs and returns an initial colour map for the
-		 * bisimulation function.
-		 *
-		 * @return {id -> colour} - a colour map
-		 */
-		function constructColourMap(){
-			var colourMap = {};
-			colourMap[0] = [constructColour(0, undefined, [])];
-			return colourMap;
-		}
-
-		/**
-		 * Constructs and returns a colouring for the specified node. Does
-		 * not consider duplicate edges in the colouring.
-		 *
-		 * @param {node} node - the node to colour
-		 * @return {colour} - the node colouring
-		 */
-		function constructColouring(node){
-			var colouring = [];
+		function constructColouring(node, nodeMap){
+			var colouring = {};
 			var edges = node.edgesFromMe;
+			var from = nodeMap[node.id].colour
 			for(var i = 0; i < edges.length; i++){
-				var from = nodeMap[node.id].colour;
 				var to = nodeMap[edges[i].to].colour;
-				var colour = constructColour(from, to, edges[i].label);
-
-				// check that the colouring does not contain the current colour
-				var match = false;
-				for(var j = 0; j < colouring.length; j++){
-					if(colouring[j].to !== colour.to){
-						continue;
-					}
-
-					if(colouring[j].label !== colour.label){
-						continue;
-					}
-
-					match = true;
-					break;
-				}
-
-				// only add colour if it is not already contained in the colouring
-				if(!match){
-					colouring.push(colour);
-				}
+				var colour = from + ' -' + edges[i].label + '> ' + to;
+				colouring[colour] = true;
 			}
+
 
 			return colouring;
 		}
 
-		/**
-		 * Constructs and returns a colour object with the specified values.
-		 *
-		 * @param {string} from - id for the node where the edge starts
-		 * @param {string} to - id for the node where the edge ends
-		 * @param {string} label - the label for the edge
-		 * @return {colour} - a colour object
-		 */
-		function constructColour(from, to, label){
-			return { from:from, to:to, label:label };
-		}
-
-		/**
-		 * Compares the specified colours together and determines if they are
-		 * equivalent. Returns true if the colours match, otherwise returns false.
-		 * This function does not take into account the id for the node where each
-		 * colour starts.
-		 *
-		 * @param {colour} colour1 - the first colour to compare
-		 * @param {colour} colour2 - the second colour to compare
-		 * @return {boolean} - whether or not the colours match
-		 */
-		function compareColours(colour1, colour2){
-			if(colour1.length !== colour2.length){
-				return false;
-			}
-
-			for(var i = 0; i < colour1.length; i++){
-				if(colour1[i].to !== colour2[i].to){
+		function compareColourings(colouring1, colouring2){
+			for(var key in colouring1){
+				if(colouring2[key] === undefined){
 					return false;
 				}
+			}
 
-				if(colour1[i].label !== colour2[i].label){
+			for(var key in colouring2){
+				if(colouring1[key] === undefined){
 					return false;
 				}
 			}
