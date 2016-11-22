f1553b57bc7702f5cbca87bfb344af0cfed56a5d
David Sheridan <dpmsheridan@hotmail.com>
2016-11-22 16:14:09 +1300

    Added some documentation to the interpreter

--- app/scripts/compiler/interpreter.js
+++ app/scripts/compiler/interpreter.js
@@ -1,26 +1,35 @@
 'use strict';
 
-var processesMap;
-var variableMap;
-var nextProcessId;
-
+/**
+ * This function is a wrapper for interpreters for each process model. Takes an
+ * array of process abstract syntax trees and delegates the interpretation of each
+ * ast to the defined process model for that process. Uses the analysis to determine
+ * if processes have had changes made to them since the last compilation.
+ *
+ * @param{Process[]} processes - an array of process abstract syntax trees
+ * @param{Identifier -> Analysis} analysis - an analysis of the currently defined processes
+ * @param{Identifier -> ProcessModel} lastProcessesMap - processes defined last compilation
+ * @param{Object} context - contains information from the main application
+ * @return{Identifier -> ProcessModel} - a mapping from identifier to process model
+ */
 function interpret(processes, analysis, lastProcessesMap, context){
-	reset();
+	const processesMap = {};
 
-	for(var i = 0; i < processes.length; i++){
+	// interpret the defined processes
+	for(let i = 0; i < processes.length; i++){
 		// check if the current process has been updated since last compilation
-		var ident = processes[i].ident.ident;
+		const ident = processes[i].ident.ident;
 		if(analysis[ident] !== undefined && analysis[ident].isUpdated){
 			// interpret the process
-			var type = processes[i].processType;
-			if(type === 'automata'){
-				interpretAutomaton(processes[i], processesMap, context);
-			}
-			else if(type === 'petrinet'){
-				interpretPetriNet(processes[i], processesMap, context);
-			}
-			else{
-				// throw error
+			switch(processes[i].processType){
+				case 'automata':
+					interpretAutomaton(processes[i], processesMap, context);
+					break;
+				case 'petrinet':
+					interpretPetriNet(processes[i], processesMap, context);
+					break;
+				default:
+					break;
 			}
 		}
 		else{
@@ -30,12 +39,6 @@ function interpret(processes, analysis, lastProcessesMap, context){
 	}
 
 	return processesMap;
-
-	function reset(){
-		processesMap = {};
-		variableMap = {};
-		nextProcessId = 0;
-	}
 }
 
 function interpretOneOff(ident, process, processType, processes, variables){
@@ -45,10 +48,10 @@ function interpretOneOff(ident, process, processType, processes, variables){
 	process = expand({ processes:[process], variableMap:variables });
 
 	if(processType === 'automata'){
-		interpretAutomaton(process.processes[0], processes, variableMap, ident, true);
+		interpretAutomaton(process.processes[0], processes, true);
 	}
 	else if(processType === 'petrinet'){
-		interpretPetriNet(process.processes[0], processes, variableMap, ident, true);
+		interpretPetriNet(process.processes[0], processes, true);
 	}
 	else{
 		// throw error
