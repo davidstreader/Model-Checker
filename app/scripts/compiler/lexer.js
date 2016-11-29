fea642bfdec7cceb39eb33c17d6525989986cffd
David Sheridan <dpmsheridan@hotmail.com>
2016-11-30 09:04:52 +1300

    Updated the lexer to handle comments

--- app/scripts/compiler/lexer.js
+++ app/scripts/compiler/lexer.js
@@ -3,13 +3,15 @@
 const Lexer = {
 	actionLabel: '[a-z][A-Za-z0-9_]*',
 	identifier: '[A-Z][A-Za-z0-9_]*',
-	integer: '[1-9][0-9]*',
+	integer: '[0-9][0-9]*',
 	processTypes: {'automata':true, 'petrinet':true },
-	functions: {'abs': true, 'simp':true, 'tokenRule':true },
+	functions: {'abs': true, 'simp':true, 'tokenRule':true, 'toPetriNet':true },
 	terminals: { 'STOP':true, 'ERROR': true },
 	keywords: { 'const':true, 'range':true, 'set':true, 'if':true, 'then':true, 'else':true, 'when':true, 'forall':true },
 	symbols: '(\\.\\.|\\.|,|:|\\[|\\]|\\(|\\)|\\{|\\}|->|\\\\|@)',
 	operators: '(\\|\\||\\||&&|&|\\^|==|=|!=|<<|<=|<|>>|>=|>|\\+|-|\\*|/|%|!)',
+	singleLineCommentStart: '//',
+	multiLineCommentStart: '/\\*',
 
 	/**
 	 * Passes over the specified code block and converts it into
@@ -26,8 +28,9 @@ const Lexer = {
 		// loop through the code and construct tokens
 		while(code.length !== 0){
 			gobbleWhitespace();
+			gobbleComments.bind(this)();
 
-      // construct the start position for the current token
+     	// construct the start position for the current token
       const start = new Position(line, column);
 
 			let value;
@@ -38,18 +41,18 @@ const Lexer = {
         // construct the end point for the current token
         const end = new Position(line, column);
 
-        // construct the location in the code of this token
-        const location = new Location(start, end);
+		    // construct the location in the code of this token
+		    const location = new Location(start, end);
 
-        // determine if the action tokenised was actally a process type
+		    // determine if the action tokenised was actally a process type
 				if(this.processTypes[value] !== undefined){
 					tokens.push(new Token('process-type', value, location));
 				}
-        // determine if the action tokenised was actually a function
+		    // determine if the action tokenised was actually a function
 				else if(this.functions[value] !== undefined){
 					tokens.push(new Token('function', value, location));
 				}
-        // determine if the action tokenised was actually a keyword
+		    // determine if the action tokenised was actually a keyword
 				else if(this.keywords[value] !== undefined){
 					tokens.push(new Token('keyword', value, location));
 				}
@@ -87,7 +90,6 @@ const Lexer = {
 
         // construct the location in the code of this token
         const location = new Location(start, end);
-
 				tokens.push(new Token('integer', value, location));
 				continue;
 			}
@@ -118,19 +120,22 @@ const Lexer = {
 				continue;
 			}
 
-			// no match found, current character not defined by process grammar
-			const character = code.charAt(0);
-			throw new LexerException(character);
+			// no match found, check if the end of the file has been reached
+			if(code.length > 0){
+				// otherwise throw an exception
+				const character = code.charAt(0);
+				throw new LexerException(character);
+			}
 
 			break;
 		}
 
-    const end = new Position(line, column);
+   	const end = new Position(line, column);
     const location = new Location(end, end);
 		tokens.push(new Token('EOF', 'end of file', location));
 		return tokens;
 
-		// HELPER FUNCTION
+		// HELPER FUNCTIONS
 
 		/**
 		 * Attempts to match the specified regular expression at the
@@ -141,12 +146,14 @@ const Lexer = {
 		 * @return{string} value - the value that was matched, or undefined if no match
 		 */
 		function matchValue(regex){
-			const match = code.match(regex);
-			if(match !== null && match.index === 0){
-				const value = match[0];
-				column += value.length;
-				code = code.slice(value.length, code.length);
-				return value;
+			if(code.length > 0){
+				const match = code.match(regex);
+				if(match !== null && match.index === 0){
+					const value = match[0];
+					column += value.length;
+					code = code.slice(value.length, code.length);
+					return value;
+				}
 			}
 		}
 
@@ -176,6 +183,91 @@ const Lexer = {
 
 			// remove the whitespace from the code
 			code = code.slice(index, code.length);
+		}
+
+		/**
+		 * Removes any comments from the code and adjusts the current position
+		 * accordingly.
+		 */
+		function gobbleComments(){
+			// loop through and attempt to find any comments
+			while(code.length !== 0){
+				let successful = false;
+
+				// try and match a single lined comment
+				let value = matchValue(this.singleLineCommentStart);
+				if(value !== undefined){
+					gobbleSingleLineComment();
+					successful = true;
+				}
+
+				// try and match a multi lined comment
+				value = matchValue(this.multiLineCommentStart);
+				if(value !== undefined){
+					gobbleMultiLineComment();
+					successful = true;
+				}
+
+				// break if no comments were found
+				if(!successful){
+					break;
+				}
+			}
+
+			// remove any whitespace after the comments
+			gobbleWhitespace();
+		}
+
+		/**
+		 * Removes a single lined comment from the code and adjusts the current
+		 * position accordingly.
+		 */
+		function gobbleSingleLineComment(){
+			// iterate through the characters until a line break is found
+			let index = 0;
+			while(index < code.length && code.charAt(index) !== '\n'){
+				index++;
+			}
+
+			// gobble the line break
+			index++;
+
+			// remove the comment
+			code = code.slice(index, code.length);
+			
+			// update the position info
+			line++;
+			column = 0;
+		}
+
+		/**
+		 * Removes a multi lined comment from the code and adjusts the current
+		 * position accordingly.
+		 */
+		function gobbleMultiLineComment(){
+			// iterate through the characters until the end of the comment is found
+			let index = 0;
+			while(index < code.length - 1 && code.charAt(index) !== '*' && code.charAt(index + 1) !== '/'){
+				// update the position info
+				if(code.charAt(index) === '\n'){
+					line++;
+					column = 0;
+				}
+				else{
+					column++;
+				}
+
+				index++;
+			}
+
+			// gobble the end of the comment
+			index += 2;
+
+			// remove the comment
+			code = code.slice(index, code.length);
+
+			// update the position info
+			column += 2 // from the '*/' at the end of the comment
 		}
 	}
 };
