21ed293b4d3d2c091bdf71f03869101759786214
David Sheridan <dpmsheridan@hotmail.com>
2016-12-01 09:50:43 +1300

    Refactored the parallel composition algorithm for Petri nets

--- app/scripts/process-operations/petri-net/petri-net-parallel-composition.js
+++ app/scripts/process-operations/petri-net/petri-net-parallel-composition.js
@@ -1,114 +1,108 @@
 'use strict';
 
 /**
- * Performs parallel composition of the two specified petri nets. Constructs
- * a new petri net with the specified id representing the composition. Returns
+ * Performs parallel composition of the two specified Petri nets. Constructs
+ * a new Petri net with the specified id representing the composition. Returns
  * the petri net constructed by the composition.
  *
- * @param {int} id - the id of the composition
- * @param {petrinet} net1 - the first petri net
- * @param {petrinet} net2 - the second petri net
- * @return {petrinet} - the petri net formed by the composition
+ * @param {string} id - the id of the composition
+ * @param {PetriNet} net1 - the first petri net
+ * @param {PetriNet} net2 - the second petri net
+ * @return {PetriNet} - the petri net formed by the composition
  */
 function petriNetParallelComposition(id, net1, net2){
-	var net = new PetriNet(id);
+	const net = new PetriNet(id);
 
-	// add places to the composed net
-	var places = net1.places.concat(net2.places);
-	for(var i = 0; i < places.length; i++){
-		net.addPlace(places[i].id, undefined, undefined, places[i].metaData);
-		// check if this place is a start or terminal place
-		if(places[i].getMetaData('startPlace') !== undefined){
+	// construct a disjoint union of the places in the two petri nets
+	const places = net1.places.concat(net2.places);
+	for(let i = 0; i < places.length; i++){
+		const place = net.addPlace(places[i].id, places[i].metaData);
+		place.locations = places[i].locations;
+
+		// check if this is a start place
+		if(places[i].metaData.startPlace !== undefined){
 			net.addRoot(places[i].id);
 		}
 	}
 
-	var labelSets1 = net1.labelSets;
-	var labelSets2 = net2.labelSets;
+	const alphabet1 = net1.alphabet;
+	const alphabet2 = net2.alphabet;
 
-	for(var action in labelSets1){
-		var transitions = labelSets1[action];
-		for(var i = 0; i < transitions.length; i++){
-			var incoming = transitions[i].incomingPlaces.map(id => net.getPlace(id));
-			var outgoing = transitions[i].outgoingPlaces.map(id => net.getPlace(id));
-			var locations = transitions[i].locations;
-
-			// check if the actions are synced
-			if(labelSets2[action] !== undefined){
-				var synced = labelSets2[action];
-				for(var j = 0; j < synced.length; j++){
-					incoming = incoming.concat(synced[j].incomingPlaces.map(id => net.getPlace(id)));
-					outgoing = outgoing.concat(synced[j].outgoingPlaces.map(id => net.getPlace(id)));
-					for(var id in synced[j].locations){
-						locations[id] = true;
-					}
-				}
-
-
-			}
-
-			incoming = processPlaceArray(incoming);
-			outgoing = processPlaceArray(outgoing);
-			var transition = net.addTransition(transitions[i].id, action, incoming, outgoing);
-			transition.locations = locations;
+	// construct sets of synchronised and non-synchronised actions
+	const synced = {};
+	const nonSynced = {};
+	const actions = Object.keys(alphabet1).concat(Object.keys(alphabet2));
+	for(let i = 0; i < actions.length; i++){
+		const action = actions[i];
+		// check if the action is either hidden or a deadlock
+		if(action === TAU || action === DELTA){
+			nonSynced[action] = true;
+		}
+		// check if both proecsses perfrom the action
+		else if(alphabet1[action] !== undefined && alphabet2[action] !== undefined){
+			synced[action] = true;
+		}
+		else{
+			nonSynced[action] = true;
 		}
 	}
 
-	for(var action in labelSets2){
-		if(labelSets1[action] === undefined){
-			var transitions = labelSets2[action];
-			for(var i = 0; i < transitions.length; i++){
-				// update references to incoming and outgoing places
-				var incoming = processPlaceArray(transitions[i].incomingPlaces.map(id => net.getPlace(id)));
-				var outgoing = processPlaceArray(transitions[i].outgoingPlaces.map(id => net.getPlace(id)));
-				var transition = net.addTransition(transitions[i].id, action, incoming, outgoing);
-				transition.locations = transitions[i].locations;
+	const labelSets1 = net1.labelSets;
+	const labelSets2 = net2.labelSets;
+
+	// add the non synchronised transitions
+	for(let action in nonSynced){
+		let transitions = [];
+		// attempts to add from both in case the action is hidden or deadlocked 
+		if(labelSets1[action] !== undefined){
+			transitions = transitions.concat(labelSets1[action]);
+		}
+		if(labelSets2[action] !== undefined){
+			transitions = transitions.concat(labelSets2[action]);
+		}
+
+		// add the non synchronised transitions to the petri net
+		for(let i = 0; i < transitions.length; i++){
+			// construct and add the transition
+			const id = transitions[i].id;
+			const incoming = transitions[i].incomingPlaces.map(id => net.getPlace(id));
+			const outgoing = transitions[i].outgoingPlaces.map(id => net.getPlace(id));
+			const transition = net.addTransition(id, action, incoming, outgoing);
+			transition.locations = transitions[i].locations;
+		}
+	}
+
+	// add the synchronised transitions to the petri net
+	for(let action in synced){
+		const transitions1 = labelSets1[action];
+		const transitions2 = labelSets2[action];
+
+		// sync all transitions in the first net with all the transitions
+		// in the second net that represent the same action
+		for(let i = 0; i < transitions1.length; i++){
+			const transition1 = transitions1[i];
+			
+			for(let j = 0; j < transitions2.length; j++){
+				const transition2 = transitions2[j];
+
+				// construct and add the synced transition
+				const id = transition1.id + '||' + transition2.id;
+				const incoming = transition1.incomingPlaces.concat(transition2.incomingPlaces).map(id => net.getPlace(id));
+				const outgoing = transition1.outgoingPlaces.concat(transition2.outgoingPlaces).map(id => net.getPlace(id));
+				const transition = net.addTransition(id, action, incoming, outgoing);
+
+				// update the location information for the constructed transition
+				const locations = {};
+				for(let id in transition1.locations){
+					locations[id] = true;
+				}
+				for(let id in transition2.locations){
+					locations[id] = true;
+				}
+				transition.locations = locations;
 			}
 		}
 	}
 
 	return net;
-
-	/**
-	 * Helper function for processing the parallel composition of petri nets
-	 * which syncs the incoming and outgoing places from the two specified
-	 * transitions
-	 */
-	function syncTransitions(transition1, transition2){
-		var places = transition1.outgoingPlaces.map(id => net.getPlace(id));
-		for(var i = 0; i < places.length; i++){
-			transition2.addOutgoingPlace(places[i]);
-			places[i].addIncomingTransition(transition2.id);
-		}
-		places = transition2.outgoingPlaces.map(id => net.getPlace(id));
-		for(var i = 0; i < places.length; i++){
-			transition1.addOutgoingPlace(places[i]);
-			places[i].addIncomingTransition(transition1.id);
-		}
-
-		places = transition1.incomingPlaces.map(id => net.getPlace(id));
-		for(var i = 0; i < places.length; i++){
-			transition2.addIncomingPlace(places[i]);
-			places[i].addOutgoingTransition(transition2.id);
-		}
-		places = transition2.incomingPlaces.map(id => net.getPlace(id));
-		for(var i = 0; i < places.length; i++){
-			transition1.addIncomingPlace(places[i]);
-			places[i].addOutgoingTransition(transition1.id);
-		}
-	}
-
-	function processPlaceArray(places){
-		var ids = {};
-		for(var i = 0; i < places.length; i++){
-			ids[places[i].id] = true;
-		}
-
-		var newPlaces = [];
-		for(var id in ids){
-			newPlaces.push(net.getPlace(id));
-		}
-
-		return newPlaces;
-	}
 }
