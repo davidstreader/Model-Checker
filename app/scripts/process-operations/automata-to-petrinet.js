84f990f947bd2b7974f1310b6e57d3a6e4c47e98
David Sheridan <dpmsheridan@hotmail.com>
2016-12-06 16:41:23 +1300

    Updated the automata to Petri net algorithm to handle loops in automata structure

--- app/scripts/process-operations/automata-to-petrinet.js
+++ app/scripts/process-operations/automata-to-petrinet.js
@@ -1,12 +1,14 @@
 'use strict';
 
 function automatonToPetriNet(automaton){
-	const placeMap = {};
+	const placeMap = {}; // process -> place
 	const executed = {};
-	const visitedNodes = {};
+	const stateMap = {}; // node id -> {location -> place}
+	const visited = {};
 
 	const walker = new AutomatonWalker(automaton);
 	const root = walker.root;
+	stateMap[root.id] = {};
 
 	// construct the start places for the net from the root of the automaton
 	const net = new PetriNet(automaton.id);
@@ -15,6 +17,7 @@ function automatonToPetriNet(automaton){
 		place.metaData.startPlace = 1;
 		net.addRoot(place.id);
 		placeMap[id] = place;
+		stateMap[root.id][id] = place;
 	}
 
 	// traverse through the automaton to contstruct the petri net
@@ -22,16 +25,17 @@ function automatonToPetriNet(automaton){
 	while(fringe.length !== 0){
 		const current = fringe.pop();
 
-		// mark the current node as visited
-		visitedNodes[current.id] = true;
-
 		const outgoing = walker.getOutgoingNodes(current);
 		for(let i = 0; i < outgoing.length; i++){
 			const {edge, node} = outgoing[i];
 
-			// push node to fringe if it hasn't been visited
-			if(!visitedNodes[node.id]){
+
+			// check if this node has already been visited
+			if(!visited[node.id]){
 				fringe.push(node);
+			}
+			else{
+				continue;
 			}
 
 			const edgeId = (edge.metaData.originId === undefined) ? edge.id : edge.metaData.originId;
@@ -41,22 +45,35 @@ function automatonToPetriNet(automaton){
 				continue;
 			}
 
-			// find and construct the places proceeding and preceding the transition
+			// get the incoming places to the new transition
 			const places = [];
-			const nextPlaces = [];
 			for(let id in edge.locations){
-				// preceding place
 				places.push(placeMap[id]);
-				// proceeding place
-				const place = net.addPlace();
-				placeMap[id] = place;
-				nextPlaces.push(place);
+			}
+
+			// get the outgoing places to the new transition
+			const nextPlaces = [];
+
+			if(stateMap[node.id] === undefined){
+				stateMap[node.id] = {};
+			}
+
+			for(let id in edge.locations){
+				// check if the next node has been visited
+				if(stateMap[node.id][id] === undefined){
+					const place = net.addPlace();
+					placeMap[id] = place;
+					stateMap[node.id][id] = place;
+				}
+
+				nextPlaces.push(stateMap[node.id][id])
 			}
 
 			net.addTransition(net.nextTransitionId, edge.label, places, nextPlaces);
 
 			// mark this edge as executed
 			executed[edgeId] = true;
+			visited[node.id] = true;
 		}
 	}
 
