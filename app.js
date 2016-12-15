1c9ba1b9d0199d3e3b49963c57484a1cc2d601cb
sanjay900 <sanjay.govind9@gmail.com>
2016-12-16 09:06:24 +1300

    Add some more debug for testing

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ io.on('connection', function (socket) {
     workerMap[socket.id] = worker;
     worker.onmessage = function(e) {
       if (e.data.result) {
+        socket.emit("log",{message:"Received from worker thread"});
         ack(e.data.result);
         worker.terminate();
       } else if (e.data.message) {
