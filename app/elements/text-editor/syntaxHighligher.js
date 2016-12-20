91921e3f895adcc90019114fb07a13e6b94526d6
sanjay900 <sanjay.govind9@gmail.com>
2016-12-20 22:47:46 +1300

    Add a simple syntax highlighter

---
+++ app/elements/text-editor/syntaxHighligher.js
@@ -0,0 +1,110 @@
+define('ace/mode/example', function(require, exports, module) {
+
+  var oop = require("ace/lib/oop");
+  var TextMode = require("ace/mode/text").Mode;
+  var ExampleHighlightRules = require("ace/mode/example_highlight_rules").ExampleHighlightRules;
+
+  var Mode = function() {
+    this.HighlightRules = ExampleHighlightRules;
+  };
+  oop.inherits(Mode, TextMode);
+
+  (function() {
+    // Extra logic goes here. (see below)
+  }).call(Mode.prototype);
+
+  exports.Mode = Mode;
+});
+
+define('ace/mode/example_highlight_rules', function(require, exports, module) {
+
+  var oop = require("ace/lib/oop");
+  var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+
+  var ExampleHighlightRules = function() {
+
+    var keywordMapper = this.createKeywordMapper({
+      "constant.language":
+        "STOP ERROR",
+      "keyword.operator":
+        "abs simp safe"
+    }, "text", true, " ");
+
+    this.$rules = {
+      "start" : [
+        {token : "comment.double-slash",  regex : '\\/\\/.*'},
+        {token : "comment.block",  regex : '\\/\\*', next: 'block'},
+        {token : "paren.lparen", regex : "[\\[({]"},
+        {token : "paren.rparen", regex : "[\\])}]"},
+        {token : "constant.numeric", regex: "[+-]?\\d+\\b"},
+        {token : "meta.function", regex: "automata | petrinet", next: "scope"},
+        {token : "text", regex: "const", next: "const"},
+        {caseInsensitive: true}
+      ],
+      "scope" : [
+        {token : "comment.block",  regex : '\\/\\*', next: 'blockscope'},
+        {token : "paren.lparen", regex : "[\\[({]"},
+        {token : "paren.rparen", regex : "[\\])}]", next: "start"},
+        {token : "variable.ident", regex : new RegExp(Lexer.identifier), next: "process"},
+        {defaultToken : "text"}
+      ],
+      "const" : [
+        {token : "operator", regex : '=', next  : "start"},
+        {defaultToken : "variable.constant"}
+      ],
+      "block" : [
+        {token : "comment.block", regex : '\\*\\/', next  : "start"},
+        {defaultToken : "comment.block"}
+      ],
+      "blockscope" : [
+        {token : "comment.block", regex : '\\*\\/', next  : "scope"},
+        {defaultToken : "comment.block"}
+      ],
+      "process" : [
+        {token : "meta.function", regex : "if|then|else|when|forall", next: "control"},
+        {token : "comment.double-slash",  regex : '\\/\\/.*'},
+        {token : "paren.lparen", regex : "[(]"},
+        {token : "paren.rparen", regex : "[\\])]"},
+        {token : "constant.numeric", regex: "[+-]?\\d+\\b"},
+        {token : "paren.lparen", regex : '\\[', next  : "range"},
+        {token : "string", regex : '\\/', next  : "rename"},
+        {token : "string", regex : '\\.', next  : "scope"},
+        {token : "string", regex : '\\,', next  : "scope"},
+        {token : "keyword.operator", regex: new RegExp(Lexer.operators)},
+        {token : "variable.ident", regex : new RegExp(Lexer.identifier)},
+        {token : "variable.action", regex : new RegExp(Lexer.actionLabel)},
+        {token : keywordMapper, regex : "\\b\\w+\\b"},
+        {defaultToken : "text"}
+      ],
+      "control": [
+        {token : "keyword.operator", regex: new RegExp(Lexer.operators)},
+        {token : "variable.constant", regex : new RegExp(Lexer.identifier)},
+        {token : "paren.lparen", regex : "[\\[(]"},
+        {token : "variable.action", regex : new RegExp(Lexer.actionLabel)},
+        {token : "paren.rparen", regex : "[\\])]", next: "process"},
+        {defaultToken : "text"}
+      ],
+      "range" : [
+        {token : "text", regex : '\\s|\\]:', next  : "process"},
+        {token : "keyword.operator", regex: new RegExp(Lexer.operators)},
+        {token : "paren.rparen", regex : "[\\])]"},
+        {token : "keyword.operator", regex : "\\.\\."},
+        {token : "constant.numeric", regex: "[+-]?\\d+\\b"},
+        {token : "variable.constant", regex : new RegExp(Lexer.identifier)},
+        {token : "variable.action", regex : new RegExp(Lexer.actionLabel)},
+        {defaultToken : "text"}
+      ],
+      "rename" : [
+        {token : "variable.action", regex : new RegExp(Lexer.actionLabel)},
+        {token : "paren.lparen", regex : "[\\[{(]"},
+        {token : "text", regex : '}', next  : "process"},
+        {defaultToken : "text"}
+      ],
+    };
+
+  }
+
+  oop.inherits(ExampleHighlightRules, TextHighlightRules);
+
+  exports.ExampleHighlightRules = ExampleHighlightRules;
+});
---
+++ app/elements/text-editor/theme.js
@@ -0,0 +1,131 @@
+ace.define("ace/theme/example",["require","exports","module","ace/lib/dom"], function(require, exports, module) {
+
+  exports.isDark = false;
+  exports.cssClass = "ace-test";
+  exports.cssText = ".ace-test .ace_gutter {\
+background: #f0f0f0;\
+color: #333;\
+}\
+.ace-test .ace_print-margin {\
+width: 1px;\
+background: #e8e8e8;\
+}\
+.ace-test .ace_fold {\
+background-color: #6B72E6;\
+}\
+.ace-test {\
+background-color: #FFFFFF;\
+color: black;\
+}\
+.ace-test .ace_cursor {\
+color: black;\
+}\
+.ace-test .ace_meta.ace_function{\
+color: purple;\
+}\
+.ace-test .ace_invisible {\
+color: rgb(191, 191, 191);\
+}\
+.ace-test .ace_storage,\
+.ace-test .ace_keyword {\
+color: blue;\
+}\
+.ace-test .ace_constant {\
+color: rgb(197, 6, 11);\
+}\
+.ace-test .ace_constant.ace_buildin {\
+color: rgb(88, 72, 246);\
+}\
+.ace-test .ace_constant.ace_language {\
+color: rgb(88, 92, 246);\
+}\
+.ace-test .ace_constant.ace_library {\
+color: rgb(6, 150, 14);\
+}\
+.ace-test .ace_invalid {\
+background-color: rgba(255, 0, 0, 0.1);\
+color: red;\
+}\
+.ace-test .ace_support.ace_function {\
+color: rgb(60, 76, 114);\
+}\
+.ace-test .ace_support.ace_constant {\
+color: rgb(6, 150, 14);\
+}\
+.ace-test .ace_support.ace_type,\
+.ace-test .ace_support.ace_class {\
+color: rgb(109, 121, 222);\
+}\
+.ace-test .ace_keyword.ace_operator {\
+color: rgb(104, 118, 135);\
+}\
+.ace-test .ace_string {\
+color: rgb(3, 106, 7);\
+}\
+.ace-test .ace_comment {\
+color: #60695e;\
+}\
+.ace-test .ace_constant.ace_numeric {\
+color: rgb(0, 0, 205);\
+}\
+.ace-test .ace_variable.ace_action {\
+color: rgb(0, 0, 205);\
+}\
+.ace-test .ace_variable {\
+color: rgb(49, 132, 149);\
+}\
+.ace-test .ace_variable.ace_constant{\
+color: rgb(220, 21, 21);\
+}\
+.ace-test .ace_xml-pe {\
+color: rgb(104, 104, 91);\
+}\
+.ace-test .ace_entity.ace_name.ace_function {\
+color: #0000A2;\
+}\
+.ace-test .ace_heading {\
+color: rgb(12, 7, 255);\
+}\
+.ace-test .ace_list {\
+color:rgb(185, 6, 144);\
+}\
+.ace-test .ace_meta.ace_tag {\
+color:rgb(0, 22, 142);\
+}\
+.ace-test .ace_string.ace_regex {\
+color: rgb(255, 0, 0)\
+}\
+.ace-test .ace_marker-layer .ace_selection {\
+background: rgb(181, 213, 255);\
+}\
+.ace-test.ace_multiselect .ace_selection.ace_start {\
+box-shadow: 0 0 3px 0px white;\
+}\
+.ace-test .ace_marker-layer .ace_step {\
+background: rgb(252, 255, 0);\
+}\
+.ace-test .ace_marker-layer .ace_stack {\
+background: rgb(164, 229, 101);\
+}\
+.ace-test .ace_marker-layer .ace_bracket {\
+margin: -1px 0 0 -1px;\
+border: 1px solid rgb(192, 192, 192);\
+}\
+.ace-test .ace_marker-layer .ace_active-line {\
+background: rgba(0, 0, 0, 0.07);\
+}\
+.ace-test .ace_gutter-active-line {\
+background-color : #dcdcdc;\
+}\
+.ace-test .ace_marker-layer .ace_selected-word {\
+background: rgb(250, 250, 255);\
+border: 1px solid rgb(200, 200, 250);\
+}\
+.ace-test .ace_indent-guide {\
+background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4////f4bLly//BwAmVgd1/w11/gAAAABJRU5ErkJggg==\") right repeat-y;\
+}\
+";
+
+  var dom = require("ace/lib/dom");
+  dom.importCssString(exports.cssText, exports.cssClass);
+});
