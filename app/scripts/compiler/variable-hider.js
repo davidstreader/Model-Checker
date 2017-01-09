14006d04d9e3e178cfdd9c8815562e984a6d668c
sanjay900 <sanjay.govind9@gmail.com>
2017-01-10 10:35:26 +1300

    Fix duplicated actions when hiding a variable.

--- app/scripts/compiler/variable-hider.js
+++ app/scripts/compiler/variable-hider.js
@@ -1,18 +1,21 @@
 function hideVariables(processes) {
   for (let process in processes) {
     const processMap = {};
-    processes[process] = constructMap(processes[process],processMap);
-    processes[process] = hideInProcess(processes[process],processMap);
+    const actionMap = [];
+    constructMap(processes[process],processMap);
+    processes[process] = hideInProcess(processes[process],processMap,undefined,actionMap);
   }
 
   return processes;
 
-  function constructMap(process,processMap, variableSet, ranges) {
+  function constructMap(process,processMap, variableSet) {
     if (!variableSet && process.variables) {
       variableSet = process.variables.set;
     }
-    if (!ranges && process.ranges)
-      ranges = process.ranges;
+    const ranges = process.ranges;
+    delete process.ranges;
+    delete process.ident.ranges;
+
     if (ranges && variableSet) {
       for (let range in ranges.ranges) {
         const v = ranges.ranges[range].variable.substring(1);
@@ -30,28 +33,27 @@ function hideVariables(processes) {
     }
     if (process.local) {
       for (let local in process.local) {
-        process.local[local] = constructMap(process.local[local],processMap, variableSet, ranges);
+        constructMap(process.local[local],processMap, variableSet);
       }
     }
-    return process;
   }
-  function hideInProcess(process,processMap, variableSet) {
+  function hideInProcess(process,processMap, variableSet, actionMap) {
     if (!variableSet && process.variables) {
       variableSet = process.variables.set;
     }
     if (process.local) {
       for (let local in process.local) {
-        process.local[local] = hideInProcess(process.local[local],processMap, variableSet);
+        process.local[local] = hideInProcess(process.local[local],processMap, variableSet, actionMap);
       }
     }
-    process.process = hideNode(process.process,processMap, variableSet);
+    process.process = hideNode(process.process,processMap, variableSet, actionMap);
     if (process.local) {
       const toDel = [];
       const localMap = {};
       for (let local in process.local) {
         const l = process.local[local];
         if (localMap[l.ident.ident]) {
-          localMap[l.ident.ident].process = mergeProcess(localMap[l.ident.ident].process,l.process);
+          localMap[l.ident.ident].process = mergeProcess(localMap[l.ident.ident].process,l.process, actionMap);
           toDel.push(process.local[local]);
         } else {
           localMap[l.ident.ident] = l;
@@ -63,18 +65,18 @@ function hideVariables(processes) {
     }
     return process;
   }
-  function hideNode(node, processMap, variableSet) {
+  function hideNode(node, processMap, variableSet, actionMap) {
     switch(node.type) {
       case "choice":
       case "composite":
-        node.process1 = processBranch(node, "process1", processMap);
-        node.process2 = processBranch(node, "process2", processMap);
+        node.process1 = processBranch(node, "process1", processMap, actionMap);
+        node.process2 = processBranch(node, "process2", processMap, actionMap);
         break;
       case "sequence":
-        node.to = processBranch(node, "to", processMap);
+        node.to = processBranch(node, "to", processMap, actionMap);
         break;
       case "function":
-        node.process = processBranch(node, "process", processMap);
+        node.process = processBranch(node, "process", processMap, actionMap);
         break;
       case "identifier":
         if (processMap[node.ident]) {
@@ -99,24 +101,37 @@ function hideVariables(processes) {
     }
     return node;
   }
-  function mergeProcess(process1, process2) {
+  function mergeProcess(process1, process2, actionMap) {
     //If both nodes are identifiers, nothing needs to be done as they are identical
     if (process1.type == "identifier" && process2.type == "identifier") {
       //the identifier itself will be identical.
       return process1;
     } else {
-      if (process1.type == process2.type) {
-        if (process1.type == "sequence" && process1.to.type == process2.to.type) {
-          if (process1.to.type == "identifier" && process1.to.ident == process2.to.ident) {
-            return process1.to;
-          }
+      if (process1.type == "sequence") {
+        const loc = process1.to.location;
+        //Locations are unique. So if an action is duplicated, we can easily tell by its location being set.
+        let key = loc.start.line+","+loc.start.col+","+loc.end.line+","+loc.end.col;
+        if (actionMap[key]) {
+          //Since we dont want to include the sequence since it is duplicated, we can just return the other process
+          return process2;
         }
+        actionMap[key] = true;
+      }
+      if (process2.type == "sequence") {
+        const loc = process2.to.location;
+        //Locations are unique. So if an action is duplicated, we can easily tell by its location being set.
+        let key = loc.start.line+","+loc.start.col+","+loc.end.line+","+loc.end.col;
+        if (actionMap[key]) {
+          //Since we dont want to include the sequence since it is duplicated, we can just return the other process
+          return process1;
+        }
+        actionMap[key] = true;
       }
       //At this point, location information is hopefully not needed.
       return new ChoiceNode(process1,process2,null);
     }
   }
-  function processBranch(parent, child, processMap) {
+  function processBranch(parent, child, processMap, actionMap) {
     const ident = parent[child].ident;
     parent[child] = hideNode(parent[child], processMap);
     if (ident === undefined) return parent[child];
@@ -124,7 +139,7 @@ function hideVariables(processes) {
       const {parent:parent2,child:child2} = processMap[ident];
       const process = parent[child];
       const process2 = parent2[child2];
-      let newNode = mergeProcess(process,process2);
+      let newNode = mergeProcess(process,process2, actionMap);
       parent[child] = parent2[child2] = newNode;
       const newIdent = processMap[ident].ident;
       processMap[ident] = {
