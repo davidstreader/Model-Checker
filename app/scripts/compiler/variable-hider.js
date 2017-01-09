fbb07c3fddaeb16ebad586c9be02b15f8cc233fd
sanjay900 <sanjay.govind9@gmail.com>
2017-01-10 11:14:35 +1300

    Fix duplicated actions when hiding a variable.

--- app/scripts/compiler/variable-hider.js
+++ app/scripts/compiler/variable-hider.js
@@ -18,8 +18,12 @@ function hideVariables(processes) {
 
     if (ranges && variableSet) {
       for (let range in ranges.ranges) {
+        //Get the variable without a leading $
         const v = ranges.ranges[range].variable.substring(1);
+        //This variable exists in the hide set
         if (variableSet.indexOf(v) > -1) {
+          //Get the original identifier, and split out the number at range index.
+          //We can then join everything back up after replacing that with the variable name.
           const oldIdent = process.ident.ident;
           let tmp = oldIdent.split("[");
           const rangeIdx = parseInt(range)+1;
@@ -27,10 +31,12 @@ function hideVariables(processes) {
           tmp[rangeIdx][0] = v;
           tmp[rangeIdx] = tmp[rangeIdx].join("]");
           process.ident.ident = tmp.join("[");
+          //Store a mapping from the original name to the new name
           processMap[oldIdent] = process.ident.ident;
         }
       }
     }
+    //Construct maps for local processes also.
     if (process.local) {
       for (let local in process.local) {
         constructMap(process.local[local],processMap, variableSet);
@@ -41,15 +47,18 @@ function hideVariables(processes) {
     if (!variableSet && process.variables) {
       variableSet = process.variables.set;
     }
+    //Hide local processes first, as the main process references the local processes.
     if (process.local) {
       for (let local in process.local) {
         process.local[local] = hideInProcess(process.local[local],processMap, variableSet, actionMap);
       }
     }
+    //replace the process with a version with all hidden variables hidden
     process.process = hideNode(process.process,processMap, variableSet, actionMap);
     if (process.local) {
       const toDel = [];
       const localMap = {};
+      //Look through all local processes, and then merge together processes with identical identifiers
       for (let local in process.local) {
         const l = process.local[local];
         if (localMap[l.ident.ident]) {
@@ -65,6 +74,7 @@ function hideVariables(processes) {
     }
     return process;
   }
+  //Process a node for hiding
   function hideNode(node, processMap, variableSet, actionMap) {
     switch(node.type) {
       case "choice":
@@ -80,6 +90,7 @@ function hideVariables(processes) {
         break;
       case "identifier":
         if (processMap[node.ident]) {
+          //The process either maps to an object with the identifier as a property, or directly.
           if (typeof processMap[node.ident] == "string") {
             node.ident = processMap[node.ident];
           } else {
@@ -88,7 +99,7 @@ function hideVariables(processes) {
         }
         break;
     }
-    //Clear out hidden variables from showing their current value as it is not defined anymore.
+    //Clear out hidden variables from showing their current value in guards as it is not defined anymore.
     if (node.guardMetadata) {
       for (let it in node.guardMetadata.variables) {
         let cur = node.guardMetadata.variables[it];
@@ -119,15 +130,14 @@ function hideVariables(processes) {
       }
       if (process2.type == "sequence") {
         const loc = process2.to.location;
-        //Locations are unique. So if an action is duplicated, we can easily tell by its location being set.
         let key = loc.start.line+","+loc.start.col+","+loc.end.line+","+loc.end.col;
         if (actionMap[key]) {
-          //Since we dont want to include the sequence since it is duplicated, we can just return the other process
           return process1;
         }
         actionMap[key] = true;
       }
       //At this point, location information is hopefully not needed.
+      //the easiest way to merge two unrelated nodes is to create a choice from them.
       return new ChoiceNode(process1,process2,null);
     }
   }
@@ -136,6 +146,8 @@ function hideVariables(processes) {
     parent[child] = hideNode(parent[child], processMap);
     if (ident === undefined) return parent[child];
     if (typeof processMap[ident] === 'object') {
+      //There is already a node with this identifier. Instead of duplicating it, we merge it
+      //With the new node
       const {parent:parent2,child:child2} = processMap[ident];
       const process = parent[child];
       const process2 = parent2[child2];
@@ -148,6 +160,7 @@ function hideVariables(processes) {
         ident: newIdent
       }
     }
+    //The node needs to be marked as it will be renamed.
     if (typeof processMap[ident] == 'string') {
       const newIdent = processMap[ident];
       processMap[ident] = {
