39c59ce682a156076b6357445da63b94010b9eb2
David Sheridan <dpmsheridan@hotmail.com>
2017-01-19 16:20:18 +1300

    Added a process field to IndexNode

--- modelchecker/src/main/java/mc/compiler/ast/IndexNode.java
+++ modelchecker/src/main/java/mc/compiler/ast/IndexNode.java
@@ -1,5 +1,6 @@
 package mc.compiler.ast;
 
+import lombok.Getter;
 import mc.util.Location;
 
 public class IndexNode extends ASTNode {
@@ -7,8 +8,10 @@ public class IndexNode extends ASTNode {
 	// fields
 	private String variable;
 	private ASTNode range;
+    @Getter
+	private ASTNode process;
 
-	public IndexNode(String variable, ASTNode range, Location location){
+	public IndexNode(String variable, ASTNode range, ASTNode process, Location location){
 		super(location);
 		this.variable = variable;
 		this.range = range;
@@ -29,4 +32,5 @@ public class IndexNode extends ASTNode {
 	public void setRange(ASTNode range){
 		this.range = range;
 	}
+
 }
