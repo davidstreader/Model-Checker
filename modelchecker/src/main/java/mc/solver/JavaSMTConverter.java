6e16a766294fcadd4725ba2ff681dcaa5b79f2f8
sanjay900 <sanjay.govind9@gmail.com>
2017-01-20 12:54:36 +1300

    Try fixing linux again

--- modelchecker/src/main/java/mc/solver/JavaSMTConverter.java
+++ modelchecker/src/main/java/mc/solver/JavaSMTConverter.java
@@ -14,9 +14,7 @@ import org.sosy_lab.java_smt.api.*;
 
 import java.lang.reflect.Field;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Objects;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,6 +32,10 @@ public class JavaSMTConverter {
       Field nativePath = NativeLibraries.class.getDeclaredField("nativePath");
       nativePath.setAccessible(true);
       nativePath.set(null,Paths.get("native", arch + "-" + os));
+      HashMap<String,String> envMap = new HashMap<>();
+      envMap.put("LD_LIBRARY_PATH",Paths.get("native", arch + "-" + os).toString());
+      envMap.put("DYLD_LIBRARY_PATH",Paths.get("native", arch + "-" + os).toString());
+      setEnv(envMap);
     } catch (NoSuchFieldException | IllegalAccessException e) {
       e.printStackTrace();
     }
@@ -43,7 +45,42 @@ public class JavaSMTConverter {
       e.printStackTrace();
     }
   }
-
+  protected static void setEnv(Map<String, String> newenv)
+  {
+    try
+    {
+      Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+      Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+      theEnvironmentField.setAccessible(true);
+      Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+      env.putAll(newenv);
+      Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+      theCaseInsensitiveEnvironmentField.setAccessible(true);
+      Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
+      cienv.putAll(newenv);
+    }
+    catch (NoSuchFieldException e)
+    {
+      try {
+        Class[] classes = Collections.class.getDeclaredClasses();
+        Map<String, String> env = System.getenv();
+        for(Class cl : classes) {
+          if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+            Field field = cl.getDeclaredField("m");
+            field.setAccessible(true);
+            Object obj = field.get(env);
+            Map<String, String> map = (Map<String, String>) obj;
+            map.clear();
+            map.putAll(newenv);
+          }
+        }
+      } catch (Exception e2) {
+        e2.printStackTrace();
+      }
+    } catch (Exception e1) {
+      e1.printStackTrace();
+    }
+  }
   private static SolverContext getContext() throws InvalidConfigurationException {
     if (context == null) {
       //Initilize the solver
