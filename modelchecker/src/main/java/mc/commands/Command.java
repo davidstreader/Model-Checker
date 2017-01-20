5b07cf19e1deff9a1c2e138b70948193235a0075
sanjay900 <sanjay.govind9@gmail.com>
2017-01-21 00:12:18 +1300

    Clean up code

--- modelchecker/src/main/java/mc/commands/Command.java
+++ modelchecker/src/main/java/mc/commands/Command.java
@@ -2,12 +2,4 @@ package mc.commands;
 
 public interface Command {
   void run(String[] args);
-
-  /**
-   * Should this command pass through to the sub process?
-   * @return true for yes, false for no
-   */
-  default boolean passthrough() {
-    return false;
-  }
 }
