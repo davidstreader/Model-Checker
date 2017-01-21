f6fa9892c8cb6995a171aa2bdbfcc78a507dbfb9
sanjay900 <sanjay.govind9@gmail.com>
2017-01-22 04:16:23 +1300

    Clean gulp config, rename NodeManager to DependencyManager

--- gulpfile.js
+++ gulpfile.js
@@ -11,65 +11,15 @@
 
 // Include Gulp & tools we'll use
 const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
 let del = require('del');
-let runSequence = require('run-sequence');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
-let merge = require('merge-stream');
 const path = require('path');
 let fs = require('fs');
-let glob = require('glob');
 const historyApiFallback = require('connect-history-api-fallback');
-let packageJson = require('./package.json');
-let crypto = require('crypto');
-let polybuild = require('polybuild');
-let dom = require('gulp-dom');
-const babel = require('gulp-babel');
 const vulcanize = require('gulp-vulcanize');
 const rename = require("gulp-rename");
-const eslint = require('gulp-eslint');
 
-const AUTOPREFIXER_BROWSERS = [
-  'ie >= 10',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
-];
-
-const styleTask = function (stylesPath, srcs) {
-  return gulp.src(srcs.map(function (src) {
-    return path.join('app', stylesPath, src);
-  }))
-    .pipe($.changed(stylesPath, {extension: '.css'}))
-    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
-    .pipe(gulp.dest('.tmp/' + stylesPath))
-    .pipe($.cssmin())
-    .pipe(gulp.dest('dist/' + stylesPath))
-    .pipe($.size({title: stylesPath}));
-};
-
-// Compile and automatically prefix stylesheets
-gulp.task('styles', function() {
-  return styleTask('styles', ['**/*.css']);
-});
-
-gulp.task('elements', function() {
-  return styleTask('elements', ['**/*.css']);
-});
-
-gulp.task('scripts:es6', function() {
-  return gulp.src('app/**/*.es6.js')
-    .pipe(babel())
-    .pipe(gulp.dest('.tmp'))
-    .pipe($.uglify({preserveComments: 'some'}))
-    .pipe(gulp.dest('dist'));
-});
 gulp.task('vulcanize', function () {
   return gulp.src('app/elements/elements.html')
     .pipe(vulcanize({
@@ -83,26 +33,9 @@ gulp.task('vulcanize', function () {
     .pipe(gulp.dest('app'));
 });
 
-// Optimize images
-gulp.task('images', function() {
-  return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
-    .pipe(gulp.dest('dist/images'))
-    .pipe($.size({title: 'images'}));
-});
-
-// Copy web fonts to dist
-gulp.task('fonts', function() {
-  return gulp.src(['app/fonts/**'])
-    .pipe(gulp.dest('dist/fonts'))
-    .pipe($.size({title: 'fonts'}));
-});
 
 // Watch files for changes & reload
-gulp.task('serve', ['styles', 'elements', 'scripts:es6', 'images', 'vulcanize'], function() {
+gulp.task('serve', ['vulcanize'], function() {
   browserSync({
     port: 5000,
     notify: false,
@@ -128,7 +61,6 @@ gulp.task('serve', ['styles', 'elements', 'scripts:es6', 'images', 'vulcanize'],
   gulp.watch(['app/**/*.html'], reload);
   gulp.watch(['app/styles/**/*.css'], ['styles', reload]);
   gulp.watch(['app/elements/**/*.css'], ['elements', reload]);
-  gulp.watch(['app/**/*.es6.js'], ['scripts:es6', reload]);
   gulp.watch(['app/{scripts,elements}/**/{*.js,*.html}','!app/elements/elements.vulcanized.html'], ['vulcanize']);
   gulp.watch(['app/images/**/*'], reload);
 });
