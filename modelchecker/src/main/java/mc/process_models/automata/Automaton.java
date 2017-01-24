95dc7c2a8c8333ca0723bc44269576d3f9729078
David Sheridan <dpmsheridan@hotmail.com>
2017-01-25 10:12:36 +1300

    Fixed an issue with the combine nodes method

--- modelchecker/src/main/java/mc/process_models/automata/Automaton.java
+++ modelchecker/src/main/java/mc/process_models/automata/Automaton.java
@@ -3,9 +3,7 @@ package mc.process_models.automata;
 import mc.process_models.ProcessModel;
 import mc.process_models.ProcessModelObject;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -60,7 +58,7 @@ public class Automaton extends ProcessModelObject implements ProcessModel {
     }
 
     public AutomatonNode getNode(String id){
-        if(!nodeMap.containsKey(id)){
+        if(nodeMap.containsKey(id)){
             return nodeMap.get(id);
         }
 
@@ -93,6 +91,66 @@ public class Automaton extends ProcessModelObject implements ProcessModel {
                 .forEach(id -> edgeMap.remove(id));
         nodeMap.remove(node.getId());
         return true;
+    }
+
+    public AutomatonNode combineNodes(AutomatonNode node1, AutomatonNode node2){
+        if(!nodeMap.containsKey(node1.getId())){
+            // TODO: throw error
+        }
+
+        if(!nodeMap.containsKey(node2.getId())){
+            // TODO: throw error
+        }
+
+        AutomatonNode node = addNode();
+
+        // add the incoming and outgoing edges from both nodes to the combined nodes
+        processIncomingEdges(node, node1);
+        processIncomingEdges(node, node2);
+        processOutgoingEdges(node, node1);
+        processOutgoingEdges(node, node2);
+
+        // create an intersection of the metadata from both nodes
+        for(String key : node1.getMetaDataKeys()){
+            Object data = node1.getMetaData(key);
+
+            // check if the second node has a matching metadata key
+            if(node2.hasMetaData(key)){
+                // check that the metadata is equivalent
+                if(data.equals(node2.getMetaData(key))){
+                    // add metadata to the composed node
+                    node.addMetaData(key, data);
+
+                    if(key.equals("startNode")){
+                        setRoot(node);
+                    }
+                }
+            }
+        }
+
+        removeNode(node1);
+        removeNode(node2);
+        return node;
+    }
+
+    private void processIncomingEdges(AutomatonNode node, AutomatonNode oldNode){
+        List<AutomatonEdge> edges = oldNode.getIncomingEdges();
+        for(int i = 0; i < edges.size(); i++){
+            AutomatonEdge edge = edges.get(i);
+            node.addIncomingEdge(edge);
+            edge.setTo(node);
+            oldNode.removeIncomingEdge(edge);
+        }
+    }
+
+    private void processOutgoingEdges(AutomatonNode node, AutomatonNode oldNode){
+        List<AutomatonEdge> edges = oldNode.getOutgoingEdges();
+        for(int i = 0; i < edges.size(); i++){
+            AutomatonEdge edge = edges.get(i);
+            node.addOutgoingEdge(edge);
+            edge.setFrom(node);
+            oldNode.removeOutgoingEdge(edge);
+        }
     }
 
     public int getNodeCount(){
@@ -147,7 +205,13 @@ public class Automaton extends ProcessModelObject implements ProcessModel {
         from.addOutgoingEdge(edge);
         to.addIncomingEdge(edge);
 
+        // add edge to the edge and alphabet maps
+        if(!alphabet.containsKey(label)){
+            alphabet.put(label, new ArrayList<AutomatonEdge>());
+        }
+        alphabet.get(label).add(edge);
         edgeMap.put(id, edge);
+
         return edge;
     }
 
@@ -167,12 +231,16 @@ public class Automaton extends ProcessModelObject implements ProcessModel {
                 .collect(Collectors.toList()).size();
     }
 
+    public Set<String> getAlphabet(){
+        return alphabet.keySet();
+    }
+
     public void addAutomaton(Automaton automaton){
         automaton.getNodes().stream().forEach(node -> nodeMap.put(node.getId(), node));
         automaton.getEdges().stream().forEach(edge -> edgeMap.put(edge.getId(), edge));
-        // remove start node metadata from the root of the automaton being added
-        automaton.getRoot().removeMetaData("startNode");
     }
+
+
 
     public String getNextNodeId(){
         return getId() + ".n" + nodeId++;
@@ -180,5 +248,25 @@ public class Automaton extends ProcessModelObject implements ProcessModel {
 
     public String getNextEdgeId(){
         return getId() + ".e" + edgeId++;
+    }
+
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("automaton:{\n");
+        builder.append("\tnodes:{\n");
+        for(AutomatonNode node : nodeMap.values()){
+            builder.append("\t\t" + node.getId());
+            if(node == root){
+                builder.append("(root)");
+            }
+            builder.append("\n");
+        }
+        builder.append("\t}\n\tedges:{\n");
+        for(AutomatonEdge edge : edgeMap.values()){
+            builder.append("\t\t" + edge.getFrom().getId() + " -" + edge.getLabel() + "> " + edge.getTo().getId() + "\n");
+        }
+        builder.append("\t}\n}");
+
+        return builder.toString();
     }
 }
