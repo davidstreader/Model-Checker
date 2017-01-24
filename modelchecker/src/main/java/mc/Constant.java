ccbcce82f22dd7d31ada0fde748f9d9baf7a8d2c
David Sheridan <dpmsheridan@hotmail.com>
2017-01-24 13:44:18 +1300

    Implemented a class for storing constants

---
+++ modelchecker/src/main/java/mc/Constant.java
@@ -0,0 +1,11 @@
+package mc;
+
+/**
+ * Created by sheriddavi on 24/01/17.
+ */
+public class Constant {
+
+    public static final String HIDDEN = "HIDDEN";
+    public static final String DEADLOCK = "DEADLOCK";
+
+}
