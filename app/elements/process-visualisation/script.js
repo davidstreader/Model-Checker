2ef73092a7b52429db9d7dae48cb3d9c22c3ba81
sanjay900 <sanjay.govind9@gmail.com>
2017-01-26 20:00:44 +1300

    Don't load json unless autosave is ticked.

--- app/elements/process-visualisation/script.js
+++ app/elements/process-visualisation/script.js
@@ -136,7 +136,7 @@
         localStorage.setItem("layout", JSON.stringify(this.cy.json()));
     },
     loadJSON: function() {
-      if (localStorage.getItem("layout") !== null)
+      if (app.willSaveCookie && localStorage.getItem("layout") !== null)
         this.cy.json(JSON.parse(localStorage.getItem("layout")));
       this.loaded = true;
     },
@@ -201,59 +201,3 @@
     },
   });
 })();
-'use strict';
-
-/*!
- Event object based on jQuery events, MIT license
- https://jquery.org/license/
- https://tldrlegal.com/license/mit-license
- https://github.com/jquery/jquery/blob/master/src/event.js
- */
-
-var Event2 = function( src, props ){
-  // Allow instantiation without the 'new' keyword
-  if( !(this instanceof Event2) ){
-    return new Event2( src, props );
-  }
-  // Event object
-  if( src && src.type ){
-    this.originalEvent = src;
-    this.type = src.type;
-
-    // Events bubbling up the document may have been marked as prevented
-    // by a handler lower down the tree; reflect the correct value.
-    this.isDefaultPrevented = ( src.defaultPrevented ) ? returnTrue : returnFalse;
-
-    // Event type
-  } else {
-    this.type = src;
-  }
-
-  // Put explicitly provided properties onto the event object
-  if( props ){
-    // util.extend( this, props );
-
-    // more efficient to manually copy fields we use
-    this.type = props.type !== undefined ? props.type : this.type;
-    this.cy = props.cy;
-    this.cyTarget = props.cyTarget;
-    this.cyPosition = props.cyPosition;
-    this.cyRenderedPosition = props.cyRenderedPosition;
-    this.namespace = props.namespace;
-    this.layout = props.layout;
-    this.data = props.data;
-    this.message = props.message;
-  }
-
-  // Create a timestamp if incoming event doesn't have one
-  this.timeStamp = src && src.timeStamp || Date.now();
-};
-
-function returnFalse(){
-  return false;
-}
-
-function returnTrue(){
-  return true;
-}
-
