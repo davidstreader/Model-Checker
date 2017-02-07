c154c6a3c9ac3f8529b1495c09671c6adab16895
David Sheridan <dpmsheridan@hotmail.com>
2017-02-08 11:20:14 +1300

    Added tests for parsing forall statements

---
+++ modelchecker/src/test/java/mc/compiler/parser/ForAllTests.java
@@ -0,0 +1,185 @@
+package mc.compiler.parser;
+
+import mc.compiler.ast.*;
+import mc.exceptions.CompilationException;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by sheriddavi on 8/02/17.
+ */
+public class ForAllTests extends ParserTests {
+
+    @Test
+    public void correctForAllTest_1() throws CompilationException {
+        String input = "automata Test = (forall [i:1..2] ([i]:(a -> STOP))).";
+        ProcessNode node = constructProcessNode(input);
+        TerminalNode terminal = new TerminalNode("STOP", null);
+        SequenceNode sequence = constructSequenceNode(new String[]{"a"}, terminal);
+        ProcessRootNode process = new ProcessRootNode(sequence, "[$i]", null, null, null);
+        IndexNode index = new IndexNode("$i", new RangeNode(1, 2, null), null, null);
+        RangesNode ranges = new RangesNode(Arrays.asList(index), null);
+        ForAllStatementNode expected = new ForAllStatementNode(ranges, process, null);
+
+        if(!expected.equals(node.getProcess())){
+            fail("expecting forall nodes to be equivalent");
+        }
+    }
+
+    @Test
+    public void correctForAllTest_2() throws CompilationException {
+        String input = "automata Test = (forall [i:1..3] ([i]:(a -> STOP))).";
+        ProcessNode node = constructProcessNode(input);
+        TerminalNode terminal = new TerminalNode("STOP", null);
+        SequenceNode sequence = constructSequenceNode(new String[]{"a"}, terminal);
+        ProcessRootNode process = new ProcessRootNode(sequence, "[$i]", null, null, null);
+        IndexNode index = new IndexNode("$i", new RangeNode(1, 3, null), null, null);
+        RangesNode ranges = new RangesNode(Arrays.asList(index), null);
+        ForAllStatementNode expected = new ForAllStatementNode(ranges, process, null);
+
+        if(!expected.equals(node.getProcess())){
+            fail("expecting forall nodes to be equivalent");
+        }
+    }
+
+    @Test
+    public void correctForAllTest_3() throws CompilationException {
+        String input = "automata Test = (forall [i:{a}] ([i]:(a -> STOP))).";
+        ProcessNode node = constructProcessNode(input);
+        TerminalNode terminal = new TerminalNode("STOP", null);
+        SequenceNode sequence = constructSequenceNode(new String[]{"a"}, terminal);
+        ProcessRootNode process = new ProcessRootNode(sequence, "[$i]", null, null, null);
+        List<String> set = new ArrayList<String>(Arrays.asList("a"));
+        IndexNode index = new IndexNode("$i", new SetNode(set, null), null, null);
+        RangesNode ranges = new RangesNode(Arrays.asList(index), null);
+        ForAllStatementNode expected = new ForAllStatementNode(ranges, process, null);
+
+        if(!expected.equals(node.getProcess())){
+            fail("expecting forall nodes to be equivalent");
+        }
+    }
+
+    @Test
+    public void correctForAllTest_4() throws CompilationException {
+        String input = "automata Test = (forall [i:{a, b}] ([i]:(a -> STOP))).";
+        ProcessNode node = constructProcessNode(input);
+        TerminalNode terminal = new TerminalNode("STOP", null);
+        SequenceNode sequence = constructSequenceNode(new String[]{"a"}, terminal);
+        ProcessRootNode process = new ProcessRootNode(sequence, "[$i]", null, null, null);
+        List<String> set = new ArrayList<String>(Arrays.asList("a", "b"));
+        IndexNode index = new IndexNode("$i", new SetNode(set, null), null, null);
+        RangesNode ranges = new RangesNode(Arrays.asList(index), null);
+        ForAllStatementNode expected = new ForAllStatementNode(ranges, process, null);
+
+        if(!expected.equals(node.getProcess())){
+            fail("expecting forall nodes to be equivalent");
+        }
+    }
+
+    @Test
+    public void correctForAllTest_5() throws CompilationException {
+        String input = "automata Test = (forall [i:{a, b, c}] ([i]:(a -> STOP))).";
+        ProcessNode node = constructProcessNode(input);
+        TerminalNode terminal = new TerminalNode("STOP", null);
+        SequenceNode sequence = constructSequenceNode(new String[]{"a"}, terminal);
+        ProcessRootNode process = new ProcessRootNode(sequence, "[$i]", null, null, null);
+        List<String> set = new ArrayList<String>(Arrays.asList("a", "b", "c"));
+        IndexNode index = new IndexNode("$i", new SetNode(set, null), null, null);
+        RangesNode ranges = new RangesNode(Arrays.asList(index), null);
+        ForAllStatementNode expected = new ForAllStatementNode(ranges, process, null);
+
+        if(!expected.equals(node.getProcess())){
+            fail("expecting forall nodes to be equivalent");
+        }
+    }
+
+    @Test
+    public void correctForAllTest_6() throws CompilationException {
+        String input = "range N = 1..2 automata Test = (forall [i:N] ([i]:(a -> STOP))).";
+        ProcessNode node = constructProcessNode(input);
+        TerminalNode terminal = new TerminalNode("STOP", null);
+        SequenceNode sequence = constructSequenceNode(new String[]{"a"}, terminal);
+        ProcessRootNode process = new ProcessRootNode(sequence, "[$i]", null, null, null);
+        IndexNode index = new IndexNode("$i", new RangeNode(1, 2, null), null, null);
+        RangesNode ranges = new RangesNode(Arrays.asList(index), null);
+        ForAllStatementNode expected = new ForAllStatementNode(ranges, process, null);
+
+        if(!expected.equals(node.getProcess())){
+            fail("expecting forall nodes to be equivalent");
+        }
+    }
+
+    @Test
+    public void correctForAllTest_7() throws CompilationException {
+        String input = "range N = 1..3 automata Test = (forall [i:N] ([i]:(a -> STOP))).";
+        ProcessNode node = constructProcessNode(input);
+        TerminalNode terminal = new TerminalNode("STOP", null);
+        SequenceNode sequence = constructSequenceNode(new String[]{"a"}, terminal);
+        ProcessRootNode process = new ProcessRootNode(sequence, "[$i]", null, null, null);
+        IndexNode index = new IndexNode("$i", new RangeNode(1, 3, null), null, null);
+        RangesNode ranges = new RangesNode(Arrays.asList(index), null);
+        ForAllStatementNode expected = new ForAllStatementNode(ranges, process, null);
+
+        if(!expected.equals(node.getProcess())){
+            fail("expecting forall nodes to be equivalent");
+        }
+    }
+
+    @Test
+    public void correctForAllTest_8() throws CompilationException {
+        String input = "set N = {a} automata Test = (forall [i:N] ([i]:(a -> STOP))).";
+        ProcessNode node = constructProcessNode(input);
+        TerminalNode terminal = new TerminalNode("STOP", null);
+        SequenceNode sequence = constructSequenceNode(new String[]{"a"}, terminal);
+        ProcessRootNode process = new ProcessRootNode(sequence, "[$i]", null, null, null);
+        List<String> set = new ArrayList<String>(Arrays.asList("a"));
+        IndexNode index = new IndexNode("$i", new SetNode(set, null), null, null);
+        RangesNode ranges = new RangesNode(Arrays.asList(index), null);
+        ForAllStatementNode expected = new ForAllStatementNode(ranges, process, null);
+
+        if(!expected.equals(node.getProcess())){
+            fail("expecting forall nodes to be equivalent");
+        }
+    }
+
+    @Test
+    public void correctForAllTest_9() throws CompilationException {
+        String input = "set N = {a, b} automata Test = (forall [i:N] ([i]:(a -> STOP))).";
+        ProcessNode node = constructProcessNode(input);
+        TerminalNode terminal = new TerminalNode("STOP", null);
+        SequenceNode sequence = constructSequenceNode(new String[]{"a"}, terminal);
+        ProcessRootNode process = new ProcessRootNode(sequence, "[$i]", null, null, null);
+        List<String> set = new ArrayList<String>(Arrays.asList("a", "b"));
+        IndexNode index = new IndexNode("$i", new SetNode(set, null), null, null);
+        RangesNode ranges = new RangesNode(Arrays.asList(index), null);
+        ForAllStatementNode expected = new ForAllStatementNode(ranges, process, null);
+
+        if(!expected.equals(node.getProcess())){
+            fail("expecting forall nodes to be equivalent");
+        }
+    }
+
+    @Test
+    public void correctForAllTest_10() throws CompilationException {
+        String input = "set N = {a, b, c} automata Test = (forall [i:N] ([i]:(a -> STOP))).";
+        ProcessNode node = constructProcessNode(input);
+        TerminalNode terminal = new TerminalNode("STOP", null);
+        SequenceNode sequence = constructSequenceNode(new String[]{"a"}, terminal);
+        ProcessRootNode process = new ProcessRootNode(sequence, "[$i]", null, null, null);
+        List<String> set = new ArrayList<String>(Arrays.asList("a", "b", "c"));
+        IndexNode index = new IndexNode("$i", new SetNode(set, null), null, null);
+        RangesNode ranges = new RangesNode(Arrays.asList(index), null);
+        ForAllStatementNode expected = new ForAllStatementNode(ranges, process, null);
+
+        if(!expected.equals(node.getProcess())){
+            fail("expecting forall nodes to be equivalent");
+        }
+    }
+
+}
