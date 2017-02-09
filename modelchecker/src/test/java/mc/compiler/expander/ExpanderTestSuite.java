6da52b971a465aed4c9c78a6f0588d8054fffe97
David Sheridan <dpmsheridan@hotmail.com>
2017-02-09 14:38:13 +1300

    Implemented expander tests for process examples

--- modelchecker/src/test/java/mc/compiler/expander/ExpanderTestSuite.java
+++ modelchecker/src/test/java/mc/compiler/expander/ExpanderTestSuite.java
@@ -6,10 +6,11 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-	RangeIndexTests.class,
-	SetIndexTests.class,
-	ForAllTests.class,
-	SequenceTests.class
+	    RangeIndexTests.class,
+	    SetIndexTests.class,
+	    ForAllTests.class,
+	    SequenceTests.class,
+        ExampleTests.class
 })
 
 public class ExpanderTestSuite {
