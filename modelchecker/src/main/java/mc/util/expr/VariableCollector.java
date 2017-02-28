d8ebc5913b778fc144253f8664c12b12502dbbfd
sanjay900 <sanjay.govind9@gmail.com>
2017-02-28 22:23:09 +1300

    Cleanup variablecollector

--- modelchecker/src/main/java/mc/util/expr/VariableCollector.java
+++ modelchecker/src/main/java/mc/util/expr/VariableCollector.java
@@ -8,167 +8,25 @@ public class VariableCollector {
         if(expression instanceof VariableOperand){
             collectVariables((VariableOperand)expression, vars);
         }
-        else if(expression instanceof AdditionOperator){
-            collectVariables((AdditionOperator)expression, vars);
+        else if(expression instanceof BinaryOperator){
+            collectVariables((BinaryOperator)expression, vars);
         }
-        else if(expression instanceof SubtractionOperator){
-            collectVariables((SubtractionOperator)expression, vars);
+        else if(expression instanceof UnaryOperator){
+            collectVariables((UnaryOperator)expression, vars);
         }
-        else if(expression instanceof MultiplicationOperator){
-            collectVariables((MultiplicationOperator)expression, vars);
-        }
-        else if(expression instanceof DivisionOperator){
-            collectVariables((DivisionOperator)expression, vars);
-        }
-        else if(expression instanceof ModuloOperator){
-            collectVariables((ModuloOperator)expression, vars);
-        }
-        else if(expression instanceof LeftShiftOperator){
-            collectVariables((LeftShiftOperator)expression, vars);
-        }
-        else if(expression instanceof RightShiftOperator){
-            collectVariables((RightShiftOperator)expression, vars);
-        }
-        else if(expression instanceof OrOperator){
-            collectVariables((OrOperator)expression, vars);
-        }
-        else if(expression instanceof BitOrOperator){
-            collectVariables((BitOrOperator)expression, vars);
-        }
-        else if(expression instanceof ExclOrOperator){
-            collectVariables((ExclOrOperator)expression, vars);
-        }
-        else if(expression instanceof AndOperator){
-            collectVariables((AndOperator)expression, vars);
-        }
-        else if(expression instanceof BitAndOperator){
-            collectVariables((BitAndOperator)expression, vars);
-        }
-        else if(expression instanceof EqualityOperator){
-            collectVariables((EqualityOperator)expression, vars);
-        }
-        else if(expression instanceof NotEqualOperator){
-            collectVariables((NotEqualOperator)expression, vars);
-        }
-        else if(expression instanceof LessThanOperator){
-            collectVariables((LessThanOperator)expression, vars);
-        }
-        else if(expression instanceof LessThanEqOperator){
-            collectVariables((LessThanEqOperator)expression, vars);
-        }
-        else if(expression instanceof GreaterThanOperator){
-            collectVariables((GreaterThanOperator)expression, vars);
-        }
-        else if(expression instanceof GreaterThanEqOperator){
-            collectVariables((GreaterThanEqOperator)expression, vars);
-        }
-        else if(expression instanceof NotOperator){
-            collectVariables((NotOperator)expression, vars);
-        }
-        else if(expression instanceof BitNotOperator){
-            collectVariables((BitNotOperator)expression, vars);
-        }
+
     }
 
     private void collectVariables(VariableOperand expression, List<String> vars){
         vars.add(expression.getValue());
     }
 
-    private void collectVariables(AdditionOperator expression, List<String> vars){
+    private void collectVariables(BinaryOperator expression, List<String> vars){
         collectVariables(expression.getLeftHandSide(), vars);
         collectVariables(expression.getRightHandSide(), vars);
     }
 
-    private void collectVariables(SubtractionOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(MultiplicationOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(DivisionOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(ModuloOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(LeftShiftOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(RightShiftOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(OrOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(BitOrOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(ExclOrOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(AndOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(BitAndOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(EqualityOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(NotEqualOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(LessThanOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(LessThanEqOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(GreaterThanOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(GreaterThanEqOperator expression, List<String> vars){
-        collectVariables(expression.getLeftHandSide(), vars);
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(NotOperator expression, List<String> vars){
-        collectVariables(expression.getRightHandSide(), vars);
-    }
-
-    private void collectVariables(BitNotOperator expression, List<String> vars){
+    private void collectVariables(UnaryOperator expression, List<String> vars){
         collectVariables(expression.getRightHandSide(), vars);
     }
 
