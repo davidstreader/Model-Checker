5203b445823677639376b68726b36d9c90155d48
Sanjay <sanjay.govind9@gmail.com>
2017-07-12 02:29:15 +1200

    Move natives to resourses, automatically extract.

--- modelchecker/src/main/java/mc/util/Utils.java
+++ modelchecker/src/main/java/mc/util/Utils.java
@@ -4,40 +4,51 @@ import com.google.common.base.Ascii;
 import mc.Main;
 import org.sosy_lab.common.NativeLibraries;
 
-public class Utils {
-  @SuppressWarnings("deprecation")
-  public static String getArch() {
-    String arch = Ascii.toLowerCase(NativeLibraries.Architecture.guessVmArchitecture().name());
-    String os = Ascii.toLowerCase(NativeLibraries.OS.guessOperatingSystem().name());
-    return arch + "-" + os;
-  }
-  @SuppressWarnings("deprecation")
-  public static boolean isWin() {
-    return NativeLibraries.OS.guessOperatingSystem() == NativeLibraries.OS.WINDOWS;
-  }
-  @SuppressWarnings("deprecation")
-  public static boolean isMac() {
-    return NativeLibraries.OS.guessOperatingSystem() == NativeLibraries.OS.MACOSX;
-  }
-  public static boolean isJar() {
-    //getResource will add a jar: to the start of files inside jars.
-    return Main.class.getResource("Main.class").toString().startsWith("jar");
-  }
-  public static String getJarPath() {
-    if (!isJar()) throw new UnsupportedOperationException("The application currently is not running from a jar file.");
-    return Main.class.getResource("Main.class").toString().split("!")[0].replace("jar:file:"+(Utils.isWin()?"/":""),"");
-  }
-  /**
-   * Windows requires a .cmd appended to the node executables, linux does not.
-   * @return ".cmd" on windows or an empty string.
-   */
-  public static String getNPMExtension() {
-    if (isWin()) return ".cmd";
-    return "";
-  }
+import java.io.IOException;
 
-  public static String getNodeExtension() {
-    if (isWin()) return ".exe";
-    return "";
-  }
+public class Utils {
+    @SuppressWarnings("deprecation")
+    public static String getArch() {
+        String arch = Ascii.toLowerCase(NativeLibraries.Architecture.guessVmArchitecture().name());
+        String os = Ascii.toLowerCase(NativeLibraries.OS.guessOperatingSystem().name());
+        return arch + "-" + os;
+    }
+    @SuppressWarnings("deprecation")
+    public static boolean isWin() {
+        return NativeLibraries.OS.guessOperatingSystem() == NativeLibraries.OS.WINDOWS;
+    }
+    @SuppressWarnings("deprecation")
+    public static boolean isMac() {
+        return NativeLibraries.OS.guessOperatingSystem() == NativeLibraries.OS.MACOSX;
+    }
+    public static boolean isJavaw() {
+        try {
+            System.in.available();
+            return false;
+        } catch (IOException e) {
+            // invalid handle in case of javaw
+            return true;
+        }
+    }
+    public static boolean isJar() {
+        //getResource will add a jar: to the start of files inside jars.
+        return Main.class.getResource("Main.class").toString().startsWith("jar");
+    }
+    public static String getJarPath() {
+        if (!isJar()) throw new UnsupportedOperationException("The application currently is not running from a jar file.");
+        return Main.class.getResource("Main.class").toString().split("!")[0].replace("jar:file:"+(Utils.isWin()?"/":""),"");
+    }
+    /**
+     * Windows requires a .cmd appended to the node executables, linux does not.
+     * @return ".cmd" on windows or an empty string.
+     */
+    public static String getNPMExtension() {
+        if (isWin()) return ".cmd";
+        return "";
+    }
+
+    public static String getNodeExtension() {
+        if (isWin()) return ".exe";
+        return "";
+    }
 }
--- modelchecker/src/main/java/mc/webserver/NativesManager.java
+++ modelchecker/src/main/java/mc/webserver/NativesManager.java
@@ -1,12 +1,14 @@
 package mc.webserver;
 
 import lombok.AllArgsConstructor;
+import mc.Main;
 import mc.util.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -24,6 +26,32 @@ public class NativesManager {
      * we want to the users library folder.
      */
     public void copyNatives() {
+        if (!new File("native").exists() && Utils.isJar()) {
+            try {
+                File f =new File(NativesManager.class.getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .getPath());
+                java.util.jar.JarFile jarfile = new java.util.jar.JarFile(f);
+                java.util.Enumeration<java.util.jar.JarEntry> enu = jarfile.entries();
+                while (enu.hasMoreElements()) {
+                    java.util.jar.JarEntry je = enu.nextElement();
+                    if (!je.getName().startsWith("native")) continue;
+                    if (je.isDirectory()) {
+                        continue;
+                    }
+                    java.io.File fl = new java.io.File(je.getName());
+                    if (!fl.exists()) {
+                        fl.getParentFile().mkdirs();
+                    }
+                    java.io.InputStream is = jarfile.getInputStream(je);
+                    Files.copy(is,fl.toPath());
+                    is.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
         if (!Utils.isMac()) return;
         logger.info(""+ansi().render("@|yellow Copying natives|@"));
         String homeDir = System.getProperty("user.home");
