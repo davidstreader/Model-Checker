c79ac16a4341885a8531236eb0277e6f01713399
Sanjay <sanjay.govind9@gmail.com>
2017-08-06 02:16:13 +1200

    Add changes to update

--- modelchecker/src/main/java/mc/AutoUpdate.java
+++ modelchecker/src/main/java/mc/AutoUpdate.java
@@ -29,7 +29,7 @@ public class AutoUpdate {
                 logger.info(""+ansi().render("@|green Model checker up to date!|@"));
                 return null;
             }
-            return new String[]{obj.getJSONArray("assets").getJSONObject(0).getString("browser_download_url"),obj.getString("tag_name")};
+            return new String[]{obj.getJSONArray("assets").getJSONObject(0).getString("browser_download_url"),obj.getString("tag_name"),obj.getString("body")};
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -44,6 +44,7 @@ public class AutoUpdate {
         String[] downloadInfo = getDownloadInfo();
         if (downloadInfo == null) return;
         logger.info(""+ansi().render("@|red Update found! (Current version: "+version+", New version: "+downloadInfo[1]+")|@"));
+        logger.info(""+ansi().render("@|yellow Update changes: "+downloadInfo[2]+"|@"));
         logger.info(""+ansi().render("@|yellow Download it from: "+downloadInfo[0]+"|@"));
     }
 }
