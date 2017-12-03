63f5cb00b1744740d772de25072dba9b41440d4f
Jacob Beal <jb567@users.noreply.github.com>
2017-12-04 11:55:55 +1300

    Adds missing graph structure object

---
+++ modelchecker/clientapplication/src/main/java/mc/client/graph/GraphNode.java
@@ -0,0 +1,12 @@
+package mc.client.graph;
+
+import lombok.Data;
+
+/**
+ * Created by bealjaco on 4/12/17.
+ */
+@Data
+public class GraphNode {
+    private final String automata;
+    private final String nodeId;
+}
