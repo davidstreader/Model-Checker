b0eca542ade962c310f23f3a97d4a409e21ef49e
Jacob Beal <jb567@users.noreply.github.com>
2017-12-06 14:28:01 +1300

    document and mildly refactor the Plugin Manager

--- modelchecker/operations/src/main/java/mc/plugins/PluginManager.java
+++ modelchecker/operations/src/main/java/mc/plugins/PluginManager.java
@@ -4,25 +4,41 @@ package mc.plugins;
 import lombok.Getter;
 import org.reflections.Reflections;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PluginManager {
 
+    /**
+     * Singleton pattern
+     */
     @Getter
     private static final PluginManager instance = new PluginManager();
 
+    /**
+     * An org.reflections object that is used to retrieve classes within the {@code mc.operations} package
+     */
     private final Reflections reflection = new Reflections("mc.operations");
 
+    /**
+     * This retrieves an instance of every single valid function within the {@code mc.operations} package
+     * @return A collection of Functions
+     */
     public Collection<? extends IProcessFunction> getFunctions(){
-        return instantiateStream(reflection.getSubTypesOf(IProcessFunction.class));
+        return instantiateClasses(reflection.getSubTypesOf(IProcessFunction.class));
     }
 
-    public <V> Collection<? extends V> instantiateStream(Collection<Class<? extends V>> s){
-        return s.stream().map(aClass -> {
+    /**
+     * Converts a collection of class objects into instantiated versions
+     * @param classes a collection of class objects to be converted into new instances
+     * @param <V> the type of object the instances will extend
+     * @return An instantiated collection of objects
+     */
+    public <V> Collection<? extends V> instantiateClasses(Collection<Class<? extends V>> classes){
+        return classes.stream()
+                .distinct()
+                .map(aClass -> {
                     try {
                         return aClass.newInstance();
                     } catch (InstantiationException | IllegalAccessException e) {
@@ -31,12 +47,15 @@ public class PluginManager {
                     }
                 })
                 .filter(Objects::nonNull)
-                .collect(Collectors.toList()
-                );
+                .collect(Collectors.toList());
     }
 
+    /**
+     * This retrieves an instance of every single valid infix function within the {@code mc.operations} package
+     * @return A collection of InfixFunctions
+     */
     public Collection<? extends IProcessInfixFunction> getInfixFunctions(){
-        return instantiateStream(reflection.getSubTypesOf(IProcessInfixFunction.class));
+        return instantiateClasses(reflection.getSubTypesOf(IProcessInfixFunction.class));
     }
 
 }
