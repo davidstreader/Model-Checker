3cb54e9591db0638894bfbab9f2474bf4308a31c
Jacob Beal <jb567@users.noreply.github.com>
2017-12-13 15:35:37 +1300

    Add the sequential operator '=>' (referred to as ';' in the literature)

---
+++ modelchecker/operations/src/main/java/mc/operations/SequentialInfixFunction.java
@@ -0,0 +1,117 @@
+package mc.operations;
+
+import mc.exceptions.CompilationException;
+import mc.plugins.IProcessInfixFunction;
+import mc.process_models.automata.Automaton;
+import mc.process_models.automata.AutomatonEdge;
+import mc.process_models.automata.AutomatonNode;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SequentialInfixFunction implements IProcessInfixFunction {
+    /**
+     * A method of tracking the function
+     *
+     * @return The Human-Readable form of the function name
+     */
+    @Override
+    public String getFunctionName() {
+        return "sequential";
+    }
+
+    /**
+     * The form which the function will appear when composed in the text
+     *
+     * @return the textual notation of the infix function
+     */
+    @Override
+    public String getNotation() {
+        return "=>";
+    }
+
+    /**
+     * Execute the function
+     *
+     * @param id         the id of the resulting automaton
+     * @param automaton1 the first  automaton in the function (e.g. {@code A} in {@code A||B})
+     * @param automaton2 the second automaton in the function (e.g. {@code B} in {@code A||B})
+     * @return the resulting automaton of the operation
+     */
+    @Override
+    public Automaton compose(String id, Automaton automaton1, Automaton automaton2) throws CompilationException {
+        Automaton sequence = new Automaton(id,!Automaton.CONSTRUCT_ROOT);
+
+        //store a map to the nodes so id can be ignored
+        Map<String,AutomatonNode> automata1nodes = new HashMap<>();
+        Map<String,AutomatonNode> automata2nodes = new HashMap<>();
+
+        //copy node1 nodes across
+        automaton1.getNodes().forEach(node -> {
+            try {
+                AutomatonNode newNode = sequence.addNode();
+                newNode.copyProperties(node);
+                automata1nodes.put(node.getId(),newNode);
+
+                if(newNode.isStartNode())
+                    sequence.setRoot(newNode);
+
+            } catch (CompilationException e) {
+                e.printStackTrace();
+            }
+        });
+
+        copyAutomataEdges(sequence, automaton1, automata1nodes);
+
+        //get the stop nodes such that they can be replaced
+        Collection<AutomatonNode> stopNodes = sequence.getNodes().stream()
+                .filter(n -> "STOP".equals(n.getTerminal()))
+                .collect(Collectors.toList());
+
+        automaton2.getNodes().forEach(node -> {
+            AutomatonNode newNode = sequence.addNode();
+            newNode.copyProperties(node);
+
+            automata2nodes.put(node.getId(), newNode);
+
+            if (newNode.isStartNode()) {
+                newNode.setStartNode(false);
+                //for every stop node of automata1, get the edges that go into it, and replace it with the start node of automata2
+                for (AutomatonNode stopNode : stopNodes) {
+                    for (AutomatonEdge edge : stopNode.getIncomingEdges()) {
+                        edge.setTo(newNode);
+                        stopNode.removeIncomingEdge(edge);
+                    }
+                    sequence.removeNode(stopNode);
+                }
+            }
+        });
+
+        copyAutomataEdges(sequence, automaton2, automata2nodes);
+
+        return sequence;
+    }
+
+    /**
+     * Copies the edges from one automata to another
+     *
+     * @param writeAutomaton the automata that will have the edges copied to it
+     * @param readAutomaton  the automata that will have the edges copied from it
+     * @param nodeMap        the mapping of the ids to AutomatonNodes
+     */
+    private void copyAutomataEdges(Automaton writeAutomaton, Automaton readAutomaton, Map<String, AutomatonNode> nodeMap) {
+        readAutomaton.getEdges().forEach(e -> {
+            try {
+                AutomatonNode fromNode = nodeMap.get(e.getFrom().getId());
+                AutomatonNode toNode =   nodeMap.get(e.getTo().getId());
+                writeAutomaton.addEdge(e.getLabel(),fromNode,toNode,e.getGuard());
+            } catch (CompilationException e1) {
+                e1.printStackTrace();
+            }
+        });
+    }
+
+
+}
