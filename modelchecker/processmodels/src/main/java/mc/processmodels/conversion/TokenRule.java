2442d8aaee6a1ba1fcd75e240922a6339c6adebc
Jacob Beal <jacob.beal@ieee.org>
2018-01-29 16:03:44 +1300

    Minor changes to TokenRule for more readable code

--- modelchecker/processmodels/src/main/java/mc/processmodels/conversion/TokenRule.java
+++ modelchecker/processmodels/src/main/java/mc/processmodels/conversion/TokenRule.java
@@ -1,29 +1,51 @@
 package mc.processmodels.conversion;
 
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
 import java.util.stream.Collectors;
+import lombok.SneakyThrows;
 import mc.exceptions.CompilationException;
 import mc.processmodels.automata.Automaton;
 import mc.processmodels.automata.AutomatonNode;
 import mc.processmodels.petrinet.Petrinet;
+import mc.processmodels.petrinet.components.PetriNetEdge;
 import mc.processmodels.petrinet.components.PetriNetPlace;
 import mc.processmodels.petrinet.components.PetriNetTransition;
 
 
 /**
- * Created by smithjord3 on 23/01/18.
- * Method algorithm for converting an Petrinet to automaton
+ * This holds static methods related to the generation of automata from petrinets.
+ * <p>
+ * This is to be used such that the conversion between the two can be completed, and for being able
+ * to make petrinets and automata interoperable within process definitions.
+ *
+ * @author Jordan Smith
+ * @author David Streader
+ * @author Jacob Beal
+ * @see <a href="http://doi.org/10.1006/inco.2002.3117">
+ * The Box Algebra = Petri Nets + Process Expressions
+ * </a>
+ * @see Petrinet
+ * @see PetriNetPlace
+ * @see Automaton
+ * @see AutomatonNode
  */
 public class TokenRule {
 
-  public static Automaton tokenRule(Petrinet convertFrom) throws CompilationException {
-    Automaton outputAutomaton = new Automaton(convertFrom.getId() + " automata", false);
-
+  /**
+   * This method statically converts from a Petrinet to an Automaton visualisation of a given
+   * process.
+   *
+   * @param convertFrom the petrinet that is converted from.
+   * @return The automaton that is equivalent to {@code convertFrom} petrinet.
+   */
+  @SneakyThrows(value = {CompilationException.class})
+  public static Automaton tokenRule(Petrinet convertFrom) {
+    Automaton outputAutomaton = new Automaton(convertFrom.getId() + " automata",
+        false);
     Map<Set<PetriNetPlace>, AutomatonNode> nodeMap = new HashMap<>();
 
     AutomatonNode root = outputAutomaton.addNode();
@@ -35,11 +57,12 @@ public class TokenRule {
     Stack<Set<PetriNetPlace>> toDo = new Stack<>();
     toDo.push(convertFrom.getRoots());
 
-    Set<Set<PetriNetPlace>> previouslyVisitiedPlaces = new HashSet<>();
+    Set<Set<PetriNetPlace>> previouslyVisitedPlaces = new HashSet<>();
     int nodesCreated = 1;
+
     while (!toDo.isEmpty()) {
       Set<PetriNetPlace> currentMarking = toDo.pop();
-      if (previouslyVisitiedPlaces.contains(currentMarking)) {
+      if (previouslyVisitedPlaces.contains(currentMarking)) {
         continue;
       }
 
@@ -51,26 +74,27 @@ public class TokenRule {
 
 
       for (PetriNetTransition transition : satisfiedPostTransitions) {
-
-
         Set<PetriNetPlace> newMarking = new HashSet<>(currentMarking);
-        newMarking.removeAll(transition.pre()); // Clear out the places in the current marking which are moving token
+
+        // Clear out the places in the current marking which are moving token
+        newMarking.removeAll(transition.pre());
 
         newMarking.addAll(transition.getOutgoing().stream()
-            .map(outEdge -> (PetriNetPlace) outEdge.getTo())
+            .map(PetriNetEdge::getTo)
+            .map(PetriNetPlace.class::cast)
             .collect(Collectors.toList()));
-
 
         if (!nodeMap.containsKey(newMarking)) {
           AutomatonNode newNode = outputAutomaton.addNode();
-          newNode.setLabelNumber(nodesCreated);
+          newNode.setLabelNumber(nodesCreated++);
           nodeMap.put(newMarking, newNode);
           toDo.add(newMarking);
-          nodesCreated++;
         }
-        outputAutomaton.addEdge(transition.getLabel(), nodeMap.get(currentMarking), nodeMap.get(newMarking), null);
+
+        outputAutomaton.addEdge(transition.getLabel(), nodeMap.get(currentMarking),
+            nodeMap.get(newMarking), null);
       }
-      previouslyVisitiedPlaces.add(currentMarking);
+      previouslyVisitedPlaces.add(currentMarking);
     }
     return outputAutomaton;
   }
@@ -78,6 +102,7 @@ public class TokenRule {
   private static Set<PetriNetTransition> satisfiedTransitions(Set<PetriNetPlace> currentMarking) {
     return post(currentMarking).stream()
         .filter(transition -> currentMarking.containsAll(transition.pre()))
+        .distinct()
         .collect(Collectors.toSet());
   }
 
