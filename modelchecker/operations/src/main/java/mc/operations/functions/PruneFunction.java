a23587389da14b2d4f7a0347264f140f2f11b5da
Jacob Beal <jacob.beal@ieee.org>
2018-02-13 13:26:54 +1300

    Prune function using Reachability

--- modelchecker/operations/src/main/java/mc/operations/functions/PruneFunction.java
+++ modelchecker/operations/src/main/java/mc/operations/functions/PruneFunction.java
@@ -3,10 +3,8 @@ package mc.operations.functions;
 import com.microsoft.z3.Context;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.Stack;
 import java.util.stream.Collectors;
 import mc.exceptions.CompilationException;
 import mc.plugins.IProcessFunction;
@@ -15,9 +13,7 @@ import mc.processmodels.automata.AutomatonEdge;
 import mc.processmodels.automata.AutomatonNode;
 import mc.processmodels.automata.operations.AutomataReachability;
 import mc.processmodels.petrinet.Petrinet;
-import mc.processmodels.petrinet.components.PetriNetEdge;
-import mc.processmodels.petrinet.components.PetriNetPlace;
-import mc.processmodels.petrinet.components.PetriNetTransition;
+import mc.processmodels.petrinet.operations.PetrinetReachability;
 
 public class PruneFunction implements IProcessFunction {
   /**
@@ -121,76 +117,9 @@ public class PruneFunction implements IProcessFunction {
    * @throws CompilationException when the function fails
    */
   @Override
-  public Petrinet compose(String id, Set<String> flags, Context context, Petrinet... petrinets) throws CompilationException {
+  public Petrinet compose(String id, Set<String> flags, Context context, Petrinet... petrinets)
+      throws CompilationException {
     assert petrinets.length == 1;
-    Petrinet petri = petrinets[0].copy();
-
-    Stack<Set<PetriNetPlace>> toDo = new Stack<>();
-    toDo.push(petri.getRoots());
-
-    Set<Set<PetriNetPlace>> previouslyVisitedPlaces = new HashSet<>();
-    Set<PetriNetPlace> visitedPlaces = new HashSet<>();
-    Set<PetriNetTransition> visitedTransitions = new HashSet<>();
-
-    while (!toDo.isEmpty()) {
-      Set<PetriNetPlace> currentMarking = toDo.pop();
-
-      visitedPlaces.addAll(currentMarking);
-
-      if (previouslyVisitedPlaces.contains(currentMarking)) {
-        continue;
-      }
-
-
-      Set<PetriNetTransition> satisfiedPostTransitions = satisfiedTransitions(currentMarking);
-
-      for (PetriNetTransition transition : satisfiedPostTransitions) {
-        visitedTransitions.add(transition);
-
-        Set<PetriNetPlace> newMarking = new HashSet<>(currentMarking);
-
-
-        // Clear out the places in the current marking which are moving token
-        newMarking.removeAll(transition.pre());
-
-        newMarking.addAll(transition.getOutgoing().stream()
-            .map(PetriNetEdge::getTo)
-            .map(PetriNetPlace.class::cast)
-            .collect(Collectors.toList()));
-
-        if (!visitedPlaces.contains(newMarking)) {
-          toDo.add(newMarking);
-        }
-      }
-      previouslyVisitedPlaces.add(currentMarking);
-    }
-    Set<PetriNetPlace> placesToRemove = new HashSet<>(petri.getPlaces().values());
-    placesToRemove.removeAll(visitedPlaces);
-    Set<PetriNetTransition> transitionsToRemove = new HashSet<>(petri.getTransitions().values());
-    transitionsToRemove.removeAll(visitedTransitions);
-
-    for (PetriNetPlace p : placesToRemove) {
-      petri.removePlace(p);
-    }
-    for (PetriNetTransition t : transitionsToRemove) {
-      petri.removeTransititon(t);
-    }
-    return petri;
-  }
-
-  private static Set<PetriNetTransition> satisfiedTransitions(Set<PetriNetPlace> currentMarking) {
-    return post(currentMarking).stream()
-        .filter(transition -> currentMarking.containsAll(transition.pre()))
-        .distinct()
-        .collect(Collectors.toSet());
-  }
-
-
-  private static Set<PetriNetTransition> post(Set<PetriNetPlace> currentMarking) {
-    return currentMarking.stream()
-        .map(PetriNetPlace::post)
-        .flatMap(Set::stream)
-        .distinct()
-        .collect(Collectors.toSet());
+    return PetrinetReachability.removeUnreachableStates(petrinets[0]);
   }
 }
