93e1d9800f0146f055535bc28feda3f27aab9b8f
David Streader <david.streader@vuw.ac.nz>
2018-08-11 12:45:48 +0100

    adding FailureRefinement.java

---
+++ modelchecker/operations/src/main/java/mc/operations/FailureRefinement.java
@@ -0,0 +1,63 @@
+
+package mc.operations;
+
+import static mc.processmodels.automata.util.ColouringUtil.ColourComponent;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import mc.AcceptanceGraph;
+//import mc.BuildAcceptanceGraphs;
+import mc.exceptions.CompilationException;
+import mc.plugins.IOperationInfixFunction;
+import mc.processmodels.ProcessModel;
+import mc.processmodels.automata.Automaton;
+import mc.processmodels.automata.AutomatonEdge;
+import mc.processmodels.automata.AutomatonNode;
+import mc.processmodels.automata.util.ColouringUtil;
+
+  /**
+   * Failure refinement differs from failure equality only in the initial coloring used
+   */
+  public class FailureRefinement implements IOperationInfixFunction {
+
+
+    /**
+     * A method of tracking the function.
+     *
+     * @return The Human-Readable form of the function name
+     */
+    @Override
+    public String getFunctionName() {
+      return "FailureRefinement";
+    }
+
+    /**
+     * The form which the function will appear when composed in the text.
+     *
+     * @return the textual notation of the infix function
+     */
+    @Override
+    public String getNotation() {
+      return "<f";
+    }
+    @Override
+    public String getOperationType(){return "automata";}
+    /**
+     * Evaluate the function.
+     *
+     * @param processModels the list of automata being compared
+     * @return the resulting automaton of the operation
+     * <p>
+     * Failure equality is II-bisimulation of acceptance graphs
+     * 1. build the acceptance graphs for each automata
+     * a dfa + node to set of acceptance sets map
+     * 2. Color the nodes of the dfa acording to acceptance set equality
+     * initialise bisimulation coloring with the newly built coloring
+     */
+    @Override
+    public boolean evaluate(Collection<ProcessModel> processModels) throws CompilationException {
+      FailureEquivalence fe = new FailureEquivalence();
+      return fe.evaluate(processModels,false);
+    }
+  }
