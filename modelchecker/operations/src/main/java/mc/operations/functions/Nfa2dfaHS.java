bbdfe4608743a6afe2fa03bddfe6885dbe6d63d6
David Streader <david.streader@vuw.ac.nz>
2018-09-04 18:32:53 +0200

    About to refactor Quiescent Trace algorithm

---
+++ modelchecker/operations/src/main/java/mc/operations/functions/Nfa2dfaHS.java
@@ -0,0 +1,94 @@
+package mc.operations.functions;
+
+import com.microsoft.z3.Context;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+import mc.Constant;
+import mc.TraceType;
+import mc.exceptions.CompilationException;
+import mc.plugins.IProcessFunction;
+import mc.processmodels.MultiProcessModel;
+import mc.processmodels.automata.Automaton;
+import mc.processmodels.automata.AutomatonEdge;
+import mc.processmodels.automata.AutomatonNode;
+import mc.processmodels.petrinet.Petrinet;
+
+public class Nfa2dfaHS implements IProcessFunction {
+  /**
+   * Gets the method name when it is called (e.g. {@code abs} in {@code abs(A)}).
+   *
+   * @return the name of the function
+   */
+  @Override
+  public String getFunctionName() {
+    return "nfa2dfa";
+  }
+
+  /**
+   * Get the available flags for the function described by this interface (e.g. {@code unfair} in
+   * {@code abs{unfair}(A)}.
+   *
+   * @return a collection of available flags (note, no variables may be flags)
+   */
+  @Override
+  public Collection<String> getValidFlags() {
+    return new HashSet<>();
+  }
+
+  /**
+   * Gets the number of automata to parse into the function.
+   *
+   * @return the number of arguments
+   */
+  @Override
+  public int getNumberArguments() {
+    return 1;
+  }
+
+  boolean dfaTerminating = false;
+  /**
+   * Execute the function on automata.
+   *
+   * @param id       the id of the resulting automaton
+   * @param flags    the flags given by the function (e.g. {@code unfair} in {@code abs{unfair}(A)}
+   * @param context  the z3 context
+   * @param automata a variable number of automata taken in by the function
+   * @return the resulting automaton of the operation
+   * @throws CompilationException when the function fails
+   */
+  @Override
+  public Automaton compose(String id, Set<String> flags, Context context, Automaton... automata)
+    throws CompilationException {
+   Nfa2dfaWorks nfa2dfaWorks = new Nfa2dfaWorks();
+  return nfa2dfaWorks.compose(id,flags,context,TraceType.CompleteTrace,automata);
+  }
+
+  /**
+   * DONOTDO
+   * Execute the function on one or more petrinet.
+   *
+   * @param id        the id of the resulting petrinet
+   * @param flags     the flags given by the function (e.g. {@code unfair} in {@code abs{unfair}(A)}
+   * @param context
+   * @param petrinets the variable number of petrinets taken in by the function
+   * @return the resulting petrinet of the operation
+   * @throws CompilationException when the function fails
+   */
+  @Override
+  public Petrinet compose(String id, Set<String> flags, Context context, Petrinet... petrinets) throws CompilationException {
+    return null;
+  }
+  @Override
+  public MultiProcessModel compose(String id, Set<String> flags, Context context, MultiProcessModel... multiProcess) throws CompilationException {
+    return null;
+  }
+}
